308c6839ef77bc5011a27b673c1fe09ab8cdf1ce
davidcmm <davcandeia@gmail.com>
2015-03-11 11:40:55 -0300

    Atualizando QScore

--- scripts/analise/analisaQScore.py
+++ scripts/analise/analisaQScore.py
@@ -6,33 +6,30 @@ import sys
 #photos considered in comparisons
 allPhotos = set([])
 
-#questions aspects
-#unique = "único".decode('utf-8')
-#safe = "seguro"
-#beauty = "bonito"
-#rich = "rico"
+#possible questions
+possibleQuestions = ["agrad%C3%A1vel?", "seguro?"]
 
-unique = "3"
-safe = "2"
-beauty = "1"
-rich = "4"
-
-aspects = [unique, safe, beauty, rich]
+#possible answers
+left = 'Left'
+right = 'Right'
+notKnown = 'NotKnown'
 
 #dictionaries used to save photos comparisons results
-winsPhotosPerQuestion = {unique:{}, safe:{}, beauty:{}, rich:{}}
-lossesPhotosPerQuestion = {unique:{}, safe:{}, beauty:{}, rich:{}}
+winsPhotosPerQuestion = {possibleQuestions[0]:{}, possibleQuestions[1]:{}}
+lossesPhotosPerQuestion = {possibleQuestions[0]:{}, possibleQuestions[1]:{}}
 
 #dictionaries used to save photos win, loss and draw counters
-winsPerQuestion = {unique:{}, safe:{}, beauty:{}, rich:{}}
-lossesPerQuestion = {unique:{}, safe:{}, beauty:{}, rich:{}}
-drawsPerQuestion = {unique:{}, safe:{}, beauty:{}, rich:{}}
+winsPerQuestion = {possibleQuestions[0]:{}, possibleQuestions[1]:{}}
+lossesPerQuestion = {possibleQuestions[0]:{}, possibleQuestions[1]:{}}
+drawsPerQuestion = {possibleQuestions[0]:{}, possibleQuestions[1]:{}}
 
 def saveDraw(photo1, photo2, question):
 	"""Computing photos comparison when there is no winner."""
+	
 	draws = drawsPerQuestion[question]
 	if not draws.has_key(photo1):
 		draws[photo1] = 0
+			
 	if not draws.has_key(photo2):
 		draws[photo2] = 0
 
@@ -43,26 +40,27 @@ def saveWin(winPhoto, lossPhoto, question):
 	"""Computing photos comparison when there is a winner."""
 	#pdb.set_trace()
 	#Saving that winPhoto won against lossPhoto and updating winPhoto counter
-	winsPhotos = winsPhotosPerQuestion[question]	
-	if not winsPhotos.has_key(winPhoto):	
-		winsPhotos[winPhoto] = set([])		
-	winsPhotos[winPhoto].add(lossPhoto)
+	winsPhotosDic = winsPhotosPerQuestion[question]	
+	if not winsPhotosDic.has_key(winPhoto):	
+		winsPhotosDic[winPhoto] = set([])
+		
+	winsPhotosDic[winPhoto].add(lossPhoto)
 	
-	winsCounter = winsPerQuestion[question]
-	if not winsCounter.has_key(winPhoto):
-		winsCounter[winPhoto] = 0
-	winsCounter[winPhoto] += 1
+	winsCounterDic = winsPerQuestion[question]
+	if not winsCounterDic.has_key(winPhoto):
+		winsCounterDic[winPhoto] = 0
+	winsCounterDic[winPhoto] += 1
 
 	#Saving that lossPhoto lost against winPhoto and updating lossPhoto counter
-	lossPhotos = lossesPhotosPerQuestion[question]	
-	if not lossPhotos.has_key(lossPhoto):
-		lossPhotos[lossPhoto] = set([])
-	lossPhotos[lossPhoto].add(winPhoto)
+	lossPhotosDic = lossesPhotosPerQuestion[question]	
+	if not lossPhotosDic.has_key(lossPhoto):
+		lossPhotosDic[lossPhoto] = set([])
+	lossPhotosDic[lossPhoto].add(winPhoto)
 	
-	lossCounter = lossesPerQuestion[question]
-	if not lossCounter.has_key(lossPhoto):
-		lossCounter[lossPhoto] = 0
-	lossCounter[lossPhoto] += 1
+	lossCounterDic = lossesPerQuestion[question]
+	if not lossCounterDic.has_key(lossPhoto):
+		lossCounterDic[lossPhoto] = 0
+	lossCounterDic[lossPhoto] += 1
 
 
 def calcWiu(photo, question):
@@ -115,10 +113,12 @@ def calcLiu(photo, question):
 	Liu = ((1.0*liu) / (wiu + liu + tiu))
 	return Liu
 
-def computeQScores(allPhotos):
+def computeQScores(allPhotos, outputFileName):
 	"""Computing qscore of each photo used in experiment."""
+	outputFile = open(outputFileName, 'w')
 	
-	for question in aspects:
+	#Calculate Q-Score for each question and each photo
+	for question in possibleQuestions:
 		for photo in allPhotos:
 			
 			#if photo == "floriano_3902.jpg" and question == unique:
@@ -132,8 +132,8 @@ def computeQScores(allPhotos):
 			if winsPhotosPerQuestion[question].has_key(photo):
 				niw = len(winsPhotosPerQuestion[question][photo])
 				fac1 = 0
-				for photo1 in winsPhotosPerQuestion[question][photo]:
-					currentWiu = calcWiu(photo1, question)
+				for photoPreferredOver in winsPhotosPerQuestion[question][photo]:
+					currentWiu = calcWiu(photoPreferredOver, question)
 					fac1 += currentWiu
 				fac1 = (1.0 * fac1) / niw
 			else:
@@ -142,46 +142,119 @@ def computeQScores(allPhotos):
 			if lossesPhotosPerQuestion[question].has_key(photo):
 				nil = len(lossesPhotosPerQuestion[question][photo])
 				fac2 = 0
-				for photo2 in lossesPhotosPerQuestion[question][photo]:
-					currentLiu = calcLiu(photo2, question)
+				for photoNotPreferredOver in lossesPhotosPerQuestion[question][photo]:
+					currentLiu = calcLiu(photoNotPreferredOver, question)
 					fac2 += currentLiu
 				fac2 = (1.0 * fac2) / nil
 			else:
 				fac2 = 0
 			
 			#Computing Q-score for photo related to question			
-			if not winsPhotosPerQuestion[question].has_key(photo) and not lossesPhotosPerQuestion[question].has_key(photo) and not drawsPerQuestion[question].has_key(photo):
+			if not (winsPhotosPerQuestion[question].has_key(photo) or lossesPhotosPerQuestion[question].has_key(photo) or drawsPerQuestion[question].has_key(photo)):
 				Qiu = -1#Photo has not participated in any comparison!
 			else:			
 				Qiu = 10/3.0 * (Wiu + fac1 - fac2 + 1)
 
-			print question.strip(' \t\n\r')+ "\t" + photo.strip(' \t\n\r')+ "\t" + str(Qiu)
+			outputFile.write(question.strip(' \t\n\r')+ "\t" + photo.strip(' \t\n\r')+ "\t" + str(Qiu)+'\n')
+
+	outputFile.close()
+
+
+def evaluateFirstVote(lines, outputFileName):
+	""" Considering only the first vote for each pair of photos """
+	#pdb.set_trace()
+	photosAlreadyComputed = {possibleQuestions[0]:set([]), possibleQuestions[1]:set([])}
+
+	for line in lines:#Reading from pybossa task-run CSV
+		lineData = line.split("+")
+		userAnswer = lineData[9].strip(' \t\n\r"')
+
+		#In user answers that contain profile information, jump to comparison
+		if userAnswer[0] == '{':
+			index = userAnswer.find("Qual")
+			if index == -1:
+				raise Exception("Line with profile does not contain question: " + userAnswer)
+			userAnswer = userAnswer[index:].split(" ")
+		else:
+			userAnswer = userAnswer.split(" ")
+
+		question = userAnswer[5].strip(' \t\n\r"')
+		answer = userAnswer[6].strip(' \t\n\r"')
+		photo1 = userAnswer[7].strip(' \t\n\r"')
+		photo2 = userAnswer[8].strip(' \t\n\r"')
+		
+		if not photosAlreadyComputed[question].__contains__(photo1+" "+photo2):
+			if answer == left:
+				saveWin(photo1, photo2, question)
+			elif answer == right:
+				saveWin(photo2, photo1, question)
+			elif answer == notKnown:
+				saveDraw(photo1, photo2, question)
+
+			allPhotos.add(photo1)
+			allPhotos.add(photo2)
+			photosAlreadyComputed[question].add(photo1+" "+photo2)
+
+	computeQScores(allPhotos, outputFileName)
+
+def evaluateAllVotes(lines, outputFileName):
+	""" Considering all votes for each pair of photos """
+	votes = {possibleQuestions[0]:{}, possibleQuestions[1]:{}}
+	
+	#Reading from pybossa task-run CSV
+	for line in lines:
+		data = line.split("+")
+		answer = data[9].strip(' \t\n\r"').split(" ")
+	
+		question = answer[5].strip(' \t\n\r')
+		answer = answer[6].strip(' \t\n\r')
+		photo1 = answer[7].strip(' \t\n\r')
+		photo2 = answer[8].strip(' \t\n\r')
+		
+		#Creating votes dictionary
+		if not votes[question].has_key(photo1):
+			votes[question][photo1] = {}
+		if not votes[question][photo1].has_key(photo2):
+			votes[question][photo1][photo2] = {left:0, right:0, notKnown: 0}
+	
+		#Saving votes from task-run
+		if votes[question][photo1][photo2][left] + votes[question][photo1][photo2][right] + votes[question][photo1][photo2][notKnown] < 3: 
+			if answer == left:
+				votes[question][photo1][photo2][left] += 1
+			elif answer == right:
+				votes[question][photo1][photo2][right] += 1
+			elif answer == notKnown:
+				votes[question][photo1][photo2][notKnown] += 1
+	
+	#Evaluating votes in order to choose winning photos or ties
+	for question, qDic in votes.iteritems():
+		for photo1, photosDic in qdic.iteritems():
+			for photo2, votesDic in photosdic.iteritems():
+				leftCounter = votesDic[left]
+				rightCounter = votesDic[right]
+				tieCounter = votesDic[notKnown]	
+			
+				if leftCounter == rightCounter == tieCounter or tieCounter == 2:
+					saveDraw(photo1, photo2, question)
+				elif leftCounter == 2:
+					saveWin(photo1, photo2, question)
+				elif rightCounter == 2:
+					saveWin(photo2, photo1, question)	
+				else:
+					print "Error! " + question + " " + photo1 + " " + photo2 + " " +leftCounter + " " + rightCounter + " " + tieCounter			
+				allPhotos.add(photo1)
+				allPhotos.add(photo2)
+
+	computeQScores(allPhotos, outputFileName)
 
 
 if __name__ == "__main__":
 	if len(sys.argv) < 1:
-		print "Uso: <arquivo com dados>"
+		print "Uso: <arquivo com execuções das tarefas>"
 		sys.exit(1)
 	dataFile = open(sys.argv[1], 'r')
 	lines = dataFile.readlines()
 
-	for line in lines:
-		data = line.split("\t")
-		
-		question = data[1].strip(' \t\n\r')
-		answer = data[2].strip(' \t\n\r')
-		photo1 = data[3].strip(' \t\n\r')
-		photo2 = data[4].strip(' \t\n\r')
-			
-		if answer == 'Left':
-			saveWin(photo1, photo2, question)
-		elif answer == 'Right':
-			saveWin(photo2, photo1, question)
-		elif answer == 'NotKnown':
-			saveDraw(photo1, photo2, question)
-
-		allPhotos.add(photo1)
-		allPhotos.add(photo2)
-	
-	computeQScores(allPhotos)
-	
+	evaluateFirstVote(lines, "first.dat")
+	#evaluateAllVotes(lines, "all.dat")
+	dataFile.close()

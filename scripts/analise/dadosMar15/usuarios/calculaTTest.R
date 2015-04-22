c5320df8d1196c61fd5c68cecbe6fe13c89d3609
David <david@david-HP.(none)>
2015-04-22 16:04:38 -0300

    Adicionando boxplots dos dados

--- scripts/analise/dadosMar15/usuarios/calculaTTest.R
+++ scripts/analise/dadosMar15/usuarios/calculaTTest.R
@@ -5,6 +5,9 @@ args <- commandArgs(trailingOnly = TRUE)
 
 data1 = read.table(args[1])
 data2 = read.table(args[2])
+file1 = args[3]
+file2 = args[4]
+color = args[5]
 
 print(">> Equal")
 t.test(data1$V3, data2$V3, alternative="t")
@@ -14,3 +17,10 @@ t.test(data1$V3, data2$V3, alternative="l")
 
 print(">> Greater")
 t.test(data1$V3, data2$V3, alternative="g")
+
+#Creating boxplots of qscores per district
+pdf(paste("boxplot", file1, ".pdf"), paper="special")
+boxplot(data1$V3, main=paste("Boxplot", file1), col=color)
+
+pdf(paste("boxplot", file2, ".pdf"), paper="special")
+boxplot(data1$V3, main=paste("Boxplot", file2), col=color)

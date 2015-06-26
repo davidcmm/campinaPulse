0210422adc15d5681d7f13d4b585a6c704f916f5
davidcmm <davcandeia@gmail.com>
2015-06-26 09:59:28 -0300

    Adding filter per neighborhood

--- scripts/analise/dadosMaio15/usuarios/permut_test_david.py
+++ scripts/analise/dadosMaio15/usuarios/permut_test_david.py
@@ -11,22 +11,31 @@ def abs_median(data1, data2):
     argsrt_2 = data2.argsort()
     return np.median(np.abs(argsrt_1 - argsrt_2))
 
-def top10_abs_mean(data1, data2):
+def top10_abs_mean_naza(data1, data2):
     argsrt_1 = data1.argsort()
     argsrt_2 = data2.argsort()
 
     abs_diff = np.abs(argsrt_1 - argsrt_2)
+    top_10_1 = abs_diff[argsrt_1[-10:]]
+    top_10_2 = abs_diff[argsrt_2[-10:]]
+    
+    result = (top_10_1.mean() + top_10_2.mean()) / 2
+    return result 
+
+def top10_abs_mean_old(data1, data2):
+    argsrt_1 = data1.argsort()
+    argsrt_2 = data2.argsort()
+    abs_diff = np.abs(argsrt_1 - argsrt_2)
     top = abs_diff.argsort()[-10:]
     return abs_diff[top].mean()
 
-def ptest(df, name_g1, name_g2, pergunta, num_permut=1000):
+def ptest(df, name_g1, name_g2, pergunta, bairro=None, num_permut=100000):
     df = df[df.values[:, 1] == pergunta]
-    
-    #g1 = df[df['grupo'] == name_g1].values[:, 2] 
-    g1 = df[ (df['grupo'] == name_g1) & (df['bairro'] == 'catole') ].values[:, 2]
-    #g2 = df[df['grupo'] == name_g2].values[:, 2]
-    g2 = df[ (df['grupo'] == name_g2) & (df['bairro'] == 'catole') ].values[:, 2]
-	
+    if bairro is not None:
+        df = df[df['bairro'] == bairro]
+
+    g1 = df[df['grupo'] == name_g1].values[:, 2]
+    g2 = df[df['grupo'] == name_g2].values[:, 2]
     all_ = np.hstack((g1, g2))
     
     n_g1 = len(g1)
@@ -36,7 +45,8 @@ def ptest(df, name_g1, name_g2, pergunta, num_permut=1000):
     functions = [
             ('Mean', abs_mean),
             ('Median', abs_median),
-            ('Top10-Mean', top10_abs_mean)]
+            ('Top10-Mean-Old', top10_abs_mean_old),
+            ('Top10-Mean-Naza', top10_abs_mean_naza)]
     
     actual = np.zeros(len(functions))
     for i in xrange(len(functions)):
@@ -61,26 +71,27 @@ def ptest(df, name_g1, name_g2, pergunta, num_permut=1000):
     
     return (name_g1, name_g2, pergunta), actual, np.array(pvals), functions
 
-df = pd.read_csv('geral2.dat', sep=' ', encoding='utf8')
-#df.drop(['red', 'green', 'blue', 'hor', 'vert', 'diag'], axis=1, inplace=True)
-newdf = df.drop(['red', 'green', 'blue', 'hor', 'vert', 'diag'], axis=1)
+bairro = 'centro'
+
+df = pd.read_csv('geral.dat', sep=' ', encoding='utf8')
+df.drop(['red', 'green', 'blue', 'hor', 'vert', 'diag'], axis=1, inplace=True)
 
 all_res = []
 
-all_res.append(ptest(newdf, 'Masculino', 'Feminino', 'agradavel?'))
-all_res.append(ptest(newdf, 'Masculino', 'Feminino', 'seguro?'))
+all_res.append(ptest(df, 'Masculino', 'Feminino', 'agradavel?', bairro=bairro))
+all_res.append(ptest(df, 'Masculino', 'Feminino', 'seguro?', bairro=bairro))
 
-all_res.append(ptest(newdf, 'Jovem', 'Adulto', 'agradavel?'))
-all_res.append(ptest(newdf, 'Jovem', 'Adulto', 'seguro?'))
+all_res.append(ptest(df, 'Jovem', 'Adulto', 'agradavel?', bairro=bairro))
+all_res.append(ptest(df, 'Jovem', 'Adulto', 'seguro?', bairro=bairro))
 
-all_res.append(ptest(newdf, 'Solteiro', 'Casado', 'agradavel?'))
-all_res.append(ptest(newdf, 'Solteiro', 'Casado', 'seguro?'))
+all_res.append(ptest(df, 'Solteiro', 'Casado', 'agradavel?', bairro=bairro))
+all_res.append(ptest(df, 'Solteiro', 'Casado', 'seguro?', bairro=bairro))
 
-all_res.append(ptest(newdf, 'Baixa', 'Media', 'agradavel?'))
-all_res.append(ptest(newdf, 'Baixa', 'Media', 'seguro?'))
+all_res.append(ptest(df, 'Baixa', 'Media', 'agradavel?', bairro=bairro))
+all_res.append(ptest(df, 'Baixa', 'Media', 'seguro?', bairro=bairro))
 
-all_res.append(ptest(newdf, 'Medio', 'Pos', 'agradavel?'))
-all_res.append(ptest(newdf, 'Medio', 'Pos', 'seguro?'))
+all_res.append(ptest(df, 'Medio', 'Pos', 'agradavel?', bairro=bairro))
+all_res.append(ptest(df, 'Medio', 'Pos', 'seguro?', bairro=bairro))
 
 for r in all_res:
     print r[0]

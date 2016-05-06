54c90d1203ead239de66f2188e87ea040b6f3b5e
David <david@david-HP.(none)>
2016-05-06 08:50:03 -0300

    Permut test for knowing or not the neig

--- scripts/analise/dadosNov15/usuarios/permut_test_david.py
+++ scripts/analise/dadosNov15/usuarios/permut_test_david.py
@@ -142,16 +142,23 @@ def ptestFromFiles(num_it, question, neig=None, sector=None):
     if sector is not None:
 	dfOrig = dfOrig[dfOrig['setor'] == int(sector)]
 
-    men = dfOrig[dfOrig['grupo'] == 'Masculino'].values[:, 2]#Input files are sorted according to image names, so the order of QScores retrieved is paired for all groups
-    wom = dfOrig[dfOrig['grupo'] == 'Feminino'].values[:, 2]
-    you = dfOrig[dfOrig['grupo'] == 'Jovem'].values[:, 2]
-    adu = dfOrig[dfOrig['grupo'] == 'Adulto'].values[:, 2]
-    low = dfOrig[dfOrig['grupo'] == 'Baixa'].values[:, 2]
-    hig = dfOrig[dfOrig['grupo'] == 'Media'].values[:, 2]
-    sin = dfOrig[dfOrig['grupo'] == 'Solteiro'].values[:, 2]
-    mar = dfOrig[dfOrig['grupo'] == 'Casado'].values[:, 2]
-    higS = dfOrig[dfOrig['grupo'] == 'Medio'].values[:, 2]
-    pos = dfOrig[dfOrig['grupo'] == 'Pos'].values[:, 2]
+    #men = dfOrig[dfOrig['grupo'] == 'Masculino'].values[:, 2]#Input files are sorted according to image names, so the order of QScores retrieved is paired for all groups
+    #wom = dfOrig[dfOrig['grupo'] == 'Feminino'].values[:, 2]
+    #you = dfOrig[dfOrig['grupo'] == 'Jovem'].values[:, 2]
+    #adu = dfOrig[dfOrig['grupo'] == 'Adulto'].values[:, 2]
+    #low = dfOrig[dfOrig['grupo'] == 'Baixa'].values[:, 2]
+    #hig = dfOrig[dfOrig['grupo'] == 'Media'].values[:, 2]
+    #sin = dfOrig[dfOrig['grupo'] == 'Solteiro'].values[:, 2]
+    #mar = dfOrig[dfOrig['grupo'] == 'Casado'].values[:, 2]
+    #higS = dfOrig[dfOrig['grupo'] == 'Medio'].values[:, 2]
+    #pos = dfOrig[dfOrig['grupo'] == 'Pos'].values[:, 2]
+
+    knowCen = dfOrig[dfOrig['grupo'] == 'CCentro'].values[:, 2]
+    nknowCen = dfOrig[dfOrig['grupo'] == 'NCCentro'].values[:, 2]
+    knowLib = dfOrig[dfOrig['grupo'] == 'CLiberdade'].values[:, 2]
+    nknowLib = dfOrig[dfOrig['grupo'] == 'NCLiberdade'].values[:, 2]
+    knowCat = dfOrig[dfOrig['grupo'] == 'CCatole'].values[:, 2]
+    nknowCat = dfOrig[dfOrig['grupo'] == 'NCCatole'].values[:, 2]
 
     functions = [
        ('Mean', abs_mean),
@@ -159,23 +166,30 @@ def ptestFromFiles(num_it, question, neig=None, sector=None):
        ('Top10-Mean-Old', top10_abs_mean_old),
        ('Top10-Mean-Naza', top10_abs_mean_naza)]
 
-    actual = np.zeros(len(functions)*5)
+    actual = np.zeros(len(functions)*3)#*5
     index = 0
     i = 0
     while index < len(actual):
-		actual[index] = functions[i][1](men, wom)
-		actual[index+1] = functions[i][1](you, adu)
-		actual[index+2] = functions[i][1](low, hig)
-		actual[index+3] = functions[i][1](sin, mar)
-		actual[index+4] = functions[i][1](higS, pos)
-		index = index + 5
+		#actual[index] = functions[i][1](men, wom)
+		#actual[index+1] = functions[i][1](you, adu)
+		#actual[index+2] = functions[i][1](low, hig)
+		#actual[index+3] = functions[i][1](sin, mar)
+		#actual[index+4] = functions[i][1](higS, pos)
+		#index = index + 5
+		actual[index] = functions[i][1](knowCen, nknowCen)
+		actual[index+1] = functions[i][1](knowLib, nknowLib)
+		actual[index+2] = functions[i][1](knowCat, nknowCat)
+		index = index + 3
 		i = i + 1
 
-    R_gender = np.zeros(shape=(num_it, len(functions)))
-    R_marital = np.zeros(shape=(num_it, len(functions)))
-    R_age = np.zeros(shape=(num_it, len(functions)))
-    R_income = np.zeros(shape=(num_it, len(functions)))
-    R_educ = np.zeros(shape=(num_it, len(functions)))
+    #R_gender = np.zeros(shape=(num_it, len(functions)))
+    #R_marital = np.zeros(shape=(num_it, len(functions)))
+    #R_age = np.zeros(shape=(num_it, len(functions)))
+    #R_income = np.zeros(shape=(num_it, len(functions)))
+    #R_educ = np.zeros(shape=(num_it, len(functions)))
+    R_cen = np.zeros(shape=(num_it, len(functions)))
+    R_lib = np.zeros(shape=(num_it, len(functions)))
+    R_cat = np.zeros(shape=(num_it, len(functions)))
 
     #Sampled users files
     newQuestion = questionsMap[question]
@@ -188,69 +202,88 @@ def ptestFromFiles(num_it, question, neig=None, sector=None):
 	if sector is not None:
 		df = df[df['setor'] == int(sector)]
 
-	men = df[df['grupo'] == 'Masculino'].values[:, 2]
-	wom = df[df['grupo'] == 'Feminino'].values[:, 2]
-	you = df[df['grupo'] == 'Jovem'].values[:, 2]
-	adu = df[df['grupo'] == 'Adulto'].values[:, 2]
-	low = df[df['grupo'] == 'Baixa'].values[:, 2]
-	hig = df[df['grupo'] == 'Media'].values[:, 2]
-	sin = df[df['grupo'] == 'Solteiro'].values[:, 2]
-	mar = df[df['grupo'] == 'Casado'].values[:, 2]
-	higS = df[df['grupo'] == 'Medio'].values[:, 2]
-	pos = df[df['grupo'] == 'Pos'].values[:, 2]
+#	men = df[df['grupo'] == 'Masculino'].values[:, 2]
+#	wom = df[df['grupo'] == 'Feminino'].values[:, 2]
+#	you = df[df['grupo'] == 'Jovem'].values[:, 2]
+#	adu = df[df['grupo'] == 'Adulto'].values[:, 2]
+#	low = df[df['grupo'] == 'Baixa'].values[:, 2]
+#	hig = df[df['grupo'] == 'Media'].values[:, 2]
+#	sin = df[df['grupo'] == 'Solteiro'].values[:, 2]
+#	mar = df[df['grupo'] == 'Casado'].values[:, 2]
+#	higS = df[df['grupo'] == 'Medio'].values[:, 2]
+#	pos = df[df['grupo'] == 'Pos'].values[:, 2]
+
+	knowCen = df[df['grupo'] == 'CCentro'].values[:, 2]
+	nknowCen = df[df['grupo'] == 'NCCentro'].values[:, 2]
+	knowLib = df[df['grupo'] == 'CLiberdade'].values[:, 2]
+	nknowLib = df[df['grupo'] == 'NCLiberdade'].values[:, 2]
+	knowCat = df[df['grupo'] == 'CCatole'].values[:, 2]
+	nknowCat = df[df['grupo'] == 'NCCatole'].values[:, 2]
         
         for j in xrange(len(functions)):
-            R_gender[it-1][j] = functions[j][1](men, wom)
-            R_marital[it-1][j] = functions[j][1](sin, mar)
-            R_age[it-1][j] = functions[j][1](you, adu)
-            R_income[it-1][j] = functions[j][1](low, hig)
-            R_educ[it-1][j] = functions[j][1](higS, pos)
+            #R_gender[it-1][j] = functions[j][1](men, wom)
+            #R_marital[it-1][j] = functions[j][1](sin, mar)
+            #R_age[it-1][j] = functions[j][1](you, adu)
+            #R_income[it-1][j] = functions[j][1](low, hig)
+            #R_educ[it-1][j] = functions[j][1](higS, pos)
+	    R_cen[it-1][j] = functions[j][1](knowCen, nknowCen)
+	    R_lib[it-1][j] = functions[j][1](knowLib, nknowLib)
+	    R_cat[it-1][j] = functions[j][1](knowCat, nknowCat)
     
-    pvals_gender = []
-    pvals_age = []
-    pvals_marital = []
-    pvals_income = []
-    pvals_educ = []
+#    pvals_gender = []
+ #   pvals_age = []
+  #  pvals_marital = []
+   # pvals_income = []
+    #pvals_educ = []
+    pvals_cen = []
+    pvals_lib = []
+    pvals_cat = []
     index = 0
     i = 0
 
     while i < len(functions):
-	pvals_gender.append(float((R_gender[:, i] > actual[index]).sum()) / R_gender.shape[0])
-	pvals_age.append(float((R_age[:, i] > actual[index+1]).sum()) / R_age.shape[0])
-	pvals_income.append(float((R_income[:, i] > actual[index+2]).sum()) / R_income.shape[0])
-	pvals_marital.append(float((R_marital[:, i] > actual[index+3]).sum()) / R_marital.shape[0])
-	pvals_educ.append(float((R_educ[:, i] > actual[index+4]).sum()) / R_educ.shape[0])
-	index = index + 5
+#	pvals_gender.append(float((R_gender[:, i] > actual[index]).sum()) / R_gender.shape[0])
+#	pvals_age.append(float((R_age[:, i] > actual[index+1]).sum()) / R_age.shape[0])
+#	pvals_income.append(float((R_income[:, i] > actual[index+2]).sum()) / R_income.shape[0])
+#	pvals_marital.append(float((R_marital[:, i] > actual[index+3]).sum()) / R_marital.shape[0])
+#	pvals_educ.append(float((R_educ[:, i] > actual[index+4]).sum()) / R_educ.shape[0])
+	pvals_cen.append(float((R_cen[:, i] > actual[index]).sum()) / R_educ.shape[0])
+	pvals_lib.append(float((R_lib[:, i] > actual[index+1]).sum()) / R_educ.shape[0])
+	pvals_cat.append(float((R_cat[:, i] > actual[index+2]).sum()) / R_educ.shape[0])
+	index = index + 3#5
 	i = i + 1
 
-    gender = ('Masculino', 'Feminino', question), actual[0:len(actual):5], np.array(pvals_gender), functions
-    age = ('Jovem', 'Adulto', question), actual[1:len(actual):5], np.array(pvals_age), functions
-    income = ('Baixa', 'Media', question), actual[2:len(actual):5], np.array(pvals_income), functions
-    marital = ('Solteiro', 'Casado', question), actual[3:len(actual):5], np.array(pvals_marital), functions
-    educ = ('Medio', 'Pos', question), actual[4:len(actual):5], np.array(pvals_educ), functions
+    #gender = ('Masculino', 'Feminino', question), actual[0:len(actual):5], np.array(pvals_gender), functions
+    #age = ('Jovem', 'Adulto', question), actual[1:len(actual):5], np.array(pvals_age), functions
+    #income = ('Baixa', 'Media', question), actual[2:len(actual):5], np.array(pvals_income), functions
+    #marital = ('Solteiro', 'Casado', question), actual[3:len(actual):5], np.array(pvals_marital), functions
+    #educ = ('Medio', 'Pos', question), actual[4:len(actual):5], np.array(pvals_educ), functions
+    centro = ('Centro', 'NCentro', question), actual[0:len(actual):3], np.array(pvals_cen), functions
+    lib = ('Liberdade', 'NLiberdade', question), actual[0:len(actual):3], np.array(pvals_lib), functions
+    cat = ('Catole', 'NCatole', question), actual[0:len(actual):3], np.array(pvals_cat), functions
 
-    return gender, age, marital, income, educ
+    return centro, lib, cat #gender, age, marital, income, educ
 
 
 #Using sample users files
 num_it = 10000
 all_res = []
 
-all_res.append(ptestFromFiles(num_it, 'agradavel?'))
-all_res.append(ptestFromFiles(num_it, 'seguro?'))
+#all_res.append(ptestFromFiles(num_it, 'agradavel?'))
+#all_res.append(ptestFromFiles(num_it, 'seguro?'))
 
-print ">>>> General: "
+#print ">>>> General: "
 #for r in all_res:
-for index in xrange(len(all_res)):
-  data = all_res[index]#All data for one question
-  for j in xrange(len(data)):
-	  r = all_res[index][j]
-	  print r[0]
-	  for i, f in enumerate(r[-1]):
-		print f[0], r[1][i]
-		print 'pval', r[2][i]
-		print
-	  print '--'
+#for index in xrange(len(all_res)):
+#  data = all_res[index]#All data for one question
+ # for j in xrange(len(data)):
+#	  r = all_res[index][j]
+#	  print r[0]
+#	  for i, f in enumerate(r[-1]):
+#		print f[0], r[1][i]
+#		print 'pval', r[2][i]
+#		print
+#	  print '--'
 
 for neig in ['catole', 'centro', 'liberdade']:
 	all_res = []
@@ -284,23 +317,23 @@ for neig in ['catole', 'centro', 'liberdade']:
 			print
 		  print '--'
 
-for sector in ['25040090500004', '250400905000013', '250400905000060', '250400905000062', '250400905000095', '250400905000089']:
-	all_res = []
-
-	all_res.append(ptestFromFiles(num_it, 'agradavel?', sector=sector))
-	all_res.append(ptestFromFiles(num_it, 'seguro?', sector=sectorg))
-
-	print ">>>> Sector: ", sector
-	for index in xrange(len(all_res)):
-	  data = all_res[index]#All data for one question
-	  for j in xrange(len(data)):
-		  r = all_res[index][j]
-		  print r[0]
-		  for i, f in enumerate(r[-1]):
-			print f[0], r[1][i]
-			print 'pval', r[2][i]
-			print
-		  print '--'
+#for sector in ['25040090500004', '250400905000013', '250400905000060', '250400905000062', '250400905000095', '250400905000089']:
+#	all_res = []
+#
+#	all_res.append(ptestFromFiles(num_it, 'agradavel?', sector=sector))
+#	all_res.append(ptestFromFiles(num_it, 'seguro?', sector=sectorg))
+#
+#	print ">>>> Sector: ", sector
+#	for index in xrange(len(all_res)):
+#	  data = all_res[index]#All data for one question
+#	  for j in xrange(len(data)):
+#		  r = all_res[index][j]
+#		  print r[0]
+#		  for i, f in enumerate(r[-1]):
+#			print f[0], r[1][i]
+#			print 'pval', r[2][i]
+#			print
+#		  print '--'
 
 #sampling qscores from single file
 #df = pd.read_csv('geralRankAJ.dat', sep=' ', encoding='utf8')

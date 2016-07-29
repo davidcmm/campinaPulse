9e81152bb49f7922ae78b61e832433aa8b660f53
David <david@david-HP.(none)>
2016-07-29 09:46:43 -0300

    Fixing classifier input

--- scripts/analise/dadosNov15/usuarios/classifier/createInputForClassification.py
+++ scripts/analise/dadosNov15/usuarios/classifier/createInputForClassification.py
@@ -15,76 +15,86 @@ notKnown = 'NotKnown'
 
 def readTasksExecution(lines, tasksDefinitions, usersDefinitions, imagesDefinitions):
 
+	#Header
+	print "photo1\tphoto2\tchoice\tuserID\tage\tgender\tincome\teducation\tcity\tmarital\tstreet_wid1\tmov_cars1\tpark_cars1\tmov_ciclyst1\tlandscape1\tbuild_ident1\ttrees1\tbuild_height1\tdiff_build1\tpeople1\tgraffiti1\tbairro1\tstreet_wid2\tmov_cars2\tpark_cars2\tmov_ciclyst2\tlandscape2\tbuild_ident2\ttrees2\tbuild_height2\tdiff_build2\tpeople2\tgraffiti2\tbairro2"
+
 	for line in lines:
 		data = line.split("+")
 		executionID = data[0].strip(' \t\n\r')
-		taskID = lineData[3].strip(' \t\n\r"')
-		userID = lineData[4].strip(' \t\n\r"')
-		userAnswer = lineData[9].strip(' \t\n\r"')
+		taskID = data[3].strip(' \t\n\r"')
+		userID = data[4].strip(' \t\n\r"')
+		userAnswer = data[9].strip(' \t\n\r"')
 
-		userInfo = usersDefinitions[userID]#Retrieving user data
+		if usersDefinitions.has_key(userID):
+			userInfo = usersDefinitions[userID]#Retrieving user data
 
-		if executionID[0].lower() == 'n':#MaxDiff design
-			answer = json.loads(userAnswer)
-			photo1 = answer['theMost'].strip(' \t\n\r"')
-			photo2 = answer['theLess'].strip(' \t\n\r"')
+			if executionID[0].lower() == 'n':#MaxDiff design
+				answer = json.loads(userAnswer)
+				photo1 = answer['theMost'].strip(' \t\n\r"')
+				photo2 = answer['theLess'].strip(' \t\n\r"')
 
-			taskDef = tasksDefinitions[taskID]
-			photos = Set( [taskDef['url_c'].strip(' \t\n\r"'), taskDef['url_b'].strip(' \t\n\r"'), taskDef['url_a'].strip(' \t\n\r"'), taskDef['url_d'].strip(' \t\n\r"')] )
-			if(photo1 != completeTie ){
-				photos.remove(photo1)
-				photos.remove(photo2)
-			}else{
-				photo1 = photos.pop()
-				photo2 = photos.pop()
-			}
-			photo3 = photos.pop()
-			photo4 = photos.pop()
+				taskDef = tasksDefinitions[taskID]
+				photos = Set( [taskDef['url_c'].strip(' \t\n\r"'), taskDef['url_b'].strip(' \t\n\r"'), taskDef['url_a'].strip(' \t\n\r"'), taskDef['url_d'].strip(' \t\n\r"')] )
 
-			photo1Info = imagesDefinitions["/".join(photo1.split("/")[5:])]			
-			photo2Info = imagesDefinitions["/".join(photo2.split("/")[5:])]
-			photo3Info = imagesDefinitions["/".join(photo3.split("/")[5:])]
-			photo4Info = imagesDefinitions["/".join(photo4.split("/")[5:])]
+				if photo1 != completeTie:
+					photos.remove(photo1)
+					photos.remove(photo2)
+				else:
+					photo1 = photos.pop()
+					photo2 = photos.pop()
+			
+				photo3 = photos.pop()
+				photo4 = photos.pop()
 
-			#Saving votes from task-run
-			if (photo1 != completeTie){
-				print(photo1 + "\t" + photo2 + "\t" + "-1" + "\t" + " ".join(userInfo) + "\t" + " ".join(photo1Info) + "\t" + str(photo2Info) + "\t")
-				print(photo1 + "\t" + photo3 + "\t" + "-1" + "\t" + " ".join(userInfo) + "\t" + " ".join(photo1Info) + "\t" + " ".join((photo3Info) + "\t")
-				print(photo1 + "\t" + photo4 + "\t" + "-1" + "\t" + " ".join(userInfo) + "\t" + " ".join(photo1Info) + "\t" + " ".join((photo4Info) + "\t")
-				print(photo2 + "\t" + photo3 + "\t" + "1" + "\t" + " ".join(userInfo) + "\t" + " ".join(photo2Info) + "\t" + " ".join(photo3Info) + "\t")
-				print(photo2 + "\t" + photo4 + "\t" + "1" + "\t" + " ".join(userInfo) + "\t" + " ".join(photo2Info) + "\t" + " ".join(photo4Info) + "\t")
-				print(photo3 + "\t" + photo4 + "\t" + "1" + "\t" + " ".join(userInfo) + "\t" + " ".join(photo3Info) + "\t" + " ".join(photo4Info) + "\t")
-			}else{
-				print(photo1 + "\t" + photo2 + "\t" + "0" + "\t" + " ".join(userInfo) + "\t" + " ".join(photo1Info) + "\t" + " ".join(photo2Info) + "\t")
-				print(photo1 + "\t" + photo3 + "\t" + "0" + "\t" + " ".join(userInfo) + "\t" + " ".join(photo1Info) + "\t" + " ".join(photo3Info) + "\t")
-				print(photo1 + "\t" + photo4 + "\t" + "0" + "\t" + " ".join(userInfo) + "\t" + " ".join(photo1Info) + "\t" + " ".join(photo4Info) + "\t")
-				print(photo2 + "\t" + photo3 + "\t" + "0" + "\t" + " ".join(userInfo) + "\t" + " ".join(photo2Info) + "\t" + " ".join(photo3Info) + "\t")
-				print(photo2 + "\t" + photo4 + "\t" + "0" + "\t" + " ".join(userInfo) + "\t" + " ".join(photo2Info) + "\t" + " ".join(photo4Info) + "\t")
-				print(photo3 + "\t" + photo4 + "\t" + "0" + "\t" + " ".join(userInfo) + "\t" + " ".join(photo3Info) + "\t" + " ".join(photo4Info) + "\t")
-			}
-		else:#Pairwise comparison
-			if userAnswer[0] == '{':
-				index = userAnswer.find("Qual")
-				if index == -1:
-					raise Exception("Line with profile does not contain question: " + userAnswer)
-				userAnswer = userAnswer[index:].split(" ")
-			else:
-				userAnswer = userAnswer.split(" ")
+				photo1Info = imagesDefinitions["/".join(photo1.split("/")[5:])]			
+				photo2Info = imagesDefinitions["/".join(photo2.split("/")[5:])]
+				photo3Info = imagesDefinitions["/".join(photo3.split("/")[5:])]
+				photo4Info = imagesDefinitions["/".join(photo4.split("/")[5:])]
 
-			question = userAnswer[5].strip(' \t\n\r"')
-			answer = userAnswer[6].strip(' \t\n\r"')
-			photo1 = userAnswer[7].strip(' \t\n\r"')
-			photo2 = userAnswer[8].strip(' \t\n\r"')
+				#Saving votes from task-run
+				if photo1 != completeTie:
+					print(photo1 + "\t" + photo2 + "\t" + "-1" + "\t" + userID + "\t" + "\t".join(userInfo) + "\t" + "\t".join(photo1Info).strip(' \t\n\r') + "\t" + "\t".join(photo2Info).strip(' \t\n\r'))
+					print(photo1 + "\t" + photo3 + "\t" + "-1" + "\t" + userID + "\t" + "\t".join(userInfo) + "\t" + "\t".join(photo1Info).strip(' \t\n\r') + "\t" + "\t".join(photo3Info).strip(' \t\n\r'))
+					print(photo1 + "\t" + photo4 + "\t" + "-1" + "\t" + userID + "\t" + "\t".join(userInfo) + "\t" + "\t".join(photo1Info).strip(' \t\n\r') + "\t" + "\t".join(photo4Info).strip(' \t\n\r'))
+					print(photo2 + "\t" + photo3 + "\t" + "1" + "\t" + userID + "\t" + "\t".join(userInfo) + "\t" + "\t".join(photo2Info).strip(' \t\n\r') + "\t" + "\t".join(photo3Info).strip(' \t\n\r'))
+					print(photo2 + "\t" + photo4 + "\t" + "1" + "\t" + userID + "\t" + "\t".join(userInfo) + "\t" + "\t".join(photo2Info).strip(' \t\n\r') + "\t" + "\t".join(photo4Info).strip(' \t\n\r'))
+					print(photo3 + "\t" + photo4 + "\t" + "1" + "\t" + userID + "\t" + "\t".join(userInfo) + "\t" + "\t".join(photo3Info).strip(' \t\n\r') + "\t" + "\t".join(photo4Info).strip(' \t\n\r'))
+				else:
+					print(photo1 + "\t" + photo2 + "\t" + "0" + "\t" + userID + "\t" + "\t".join(userInfo) + "\t" + "\t".join(photo1Info).strip(' \t\n\r') + "\t" + "\t".join(photo2Info).strip(' \t\n\r'))
+					print(photo1 + "\t" + photo3 + "\t" + "0" + "\t" + userID + "\t" + "\t".join(userInfo) + "\t" + "\t".join(photo1Info).strip(' \t\n\r') + "\t" + "\t".join(photo3Info).strip(' \t\n\r'))
+					print(photo1 + "\t" + photo4 + "\t" + "0" + "\t" + userID + "\t" + "\t".join(userInfo) + "\t" + "\t".join(photo1Info).strip(' \t\n\r') + "\t" + "\t".join(photo4Info).strip(' \t\n\r'))
+					print(photo2 + "\t" + photo3 + "\t" + "0" + "\t" + userID + "\t" + "\t".join(userInfo) + "\t" + "\t".join(photo2Info).strip(' \t\n\r') + "\t" + "\t".join(photo3Info).strip(' \t\n\r'))
+					print(photo2 + "\t" + photo4 + "\t" + "0" + "\t" + userID + "\t" + "\t".join(userInfo) + "\t" + "\t".join(photo2Info).strip(' \t\n\r') + "\t" + "\t".join(photo4Info).strip(' \t\n\r'))
+					print(photo3 + "\t" + photo4 + "\t" + "0" + "\t" + userID + "\t" + "\t".join(userInfo) + "\t" + "\t".join(photo3Info).strip(' \t\n\r') + "\t" + "\t".join(photo4Info).strip(' \t\n\r'))
+			
+			else:#Pairwise comparison
+				if userAnswer[0] == '{':
+					index = userAnswer.find("Qual")
+					if index == -1:
+						raise Exception("Line with profile does not contain question: " + userAnswer)
+					userAnswer = userAnswer[index:].split(" ")
+				else:
+					userAnswer = userAnswer.split(" ")
 
-			photo1Info = imagesDefinitions["/".join(photo1.split("/")[5:])]			
-			photo2Info = imagesDefinitions["/".join(photo2.split("/")[5:])]
+				question = userAnswer[5].strip(' \t\n\r"')
+				answer = userAnswer[6].strip(' \t\n\r"')
+				photo1 = userAnswer[7].strip(' \t\n\r"')
+				photo2 = userAnswer[8].strip(' \t\n\r"')
+
+				photo1Info = imagesDefinitions["/".join(photo1.split("/")[5:])]			
+				photo2Info = imagesDefinitions["/".join(photo2.split("/")[5:])]
 		
-			if answer.lower() == left:
-				print(photo1 + "\t" + photo2 + "\t" + "-1" + "\t" + " ".join(userInfo) + "\t" + " ".join(photo1Info) + "\t" + " ".join(photo2Info) + "\t")
-			else if answer.lower() == right:
-				print(photo1 + "\t" + photo2 + "\t" + "1" + "\t" + " ".join(userInfo) + "\t" + " ".join(photo1Info) + "\t" + " ".join(photo2Info) + "\t")
-			else:
-				print(photo1 + "\t" + photo2 + "\t" + "0" + "\t" + " ".join(userInfo) + "\t" + " ".join(photo1Info) + "\t" + " ".join(photo2Info) + "\t")
+				if answer.lower() == left:
+					print(photo1 + "\t" + photo2 + "\t" + "-1" + "\t" + userID + "\t" + "\t".join(userInfo) + "\t" + "\t".join(photo1Info).strip(' \t\n\r') + "\t" + "\t".join(photo2Info).strip(' \t\n\r'))
+				elif answer.lower() == right:
+					print(photo1 + "\t" + photo2 + "\t" + "1" + "\t" + userID + "\t" + "\t".join(userInfo) + "\t" + "\t".join(photo1Info).strip(' \t\n\r') + "\t" + "\t".join(photo2Info).strip(' \t\n\r'))
+				else:
+					#print(photo1)
+					#print(photo2)
+					#print(" ".join(userInfo))
+					#print(photo1Info)
+					#print(" ".join(photo2Info))
+					print(photo1 + "\t" + photo2 + "\t" + "0" + "\t" + userID + "\t" + "\t".join(userInfo) + "\t" + "\t".join(photo1Info).strip(' \t\n\r') + "\t" + "\t".join(photo2Info).strip(' \t\n\r'))
 
 	return tasksDef
 
@@ -111,47 +121,55 @@ def readUsersDefinitions(linesUsers):
 		data = line.split("|")
 		userID = data[0].strip(' \t\n\r')
 		userInfoData = data[1].strip(' \t\n\r')
-	
-		#18+Masculino+Baixa (at\u00e9 R$ 1.449,99)+Gradua\u00e7\u00e3o+Campina Grande - Para\u00edba, Brasil+18+Solteiro+
-		#20+M+baixa+ensino medio+Campina Grande - PB, Brasil++solteiro+[u'cen', u'lib', u'cat']
+
 		if len(userInfoData) == 0:
-			usersDef[userID] = ['', '', '', '', '', '', '', '[]']
+			#usersDef[userID] = ['', '', '', '', '', '', '', '[]']
+			continue
 		else:
 			userInfo = userInfoData.split("+")
+			if len(userInfo[0]) == 0 and  len(userInfo[1]) == 0 and len(userInfo[2]) == 0 and len(userInfo[3]) == 0 and len(userInfo[4]) == 0 and len(userInfo[5]) == 0 and len(userInfo[6]) == 0:
+				#usersDef[userID] = ['', '', '', '', '', '', '', '[]']
+				continue
+			else:
+				gender = ""
+				if userInfo[1][0].lower() == 'm':
+					gender = "masculino"
+				else:
+					gender = "feminino"
 
-			income = ""
-			if userInfo[2] == possibleIncomesOld[0] or userInfo[2] == possibleIncomesNew[0]:
-				income = "baixa"
-			else if userInfo[2] == possibleIncomesOld[1] or userInfo[2] == possibleIncomesNew[1]:
-				income = "media baixa"
-			else if userInfo[2] == possibleIncomesOld[2] or userInfo[2] == possibleIncomesNew[2]:
-				income = "media"
-			else if userInfo[2] == possibleIncomesOld[3] or userInfo[2] == possibleIncomesNew[3]:
-				income = "media alta"
-			else if userInfo[2] == possibleIncomesOld[4] or userInfo[2] == possibleIncomesNew[4]:
-				income = "alta"
+				income = ""
+				if userInfo[2] == possibleIncomesOld[0] or userInfo[2] == possibleIncomesNew[0]:
+					income = "baixa"
+				elif userInfo[2] == possibleIncomesOld[1] or userInfo[2] == possibleIncomesNew[1]:
+					income = "media baixa"
+				elif userInfo[2] == possibleIncomesOld[2] or userInfo[2] == possibleIncomesNew[2]:
+					income = "media"
+				elif userInfo[2] == possibleIncomesOld[3] or userInfo[2] == possibleIncomesNew[3]:
+					income = "media alta"
+				elif userInfo[2] == possibleIncomesOld[4] or userInfo[2] == possibleIncomesNew[4]:
+					income = "alta"
 
-			educ = ""
-			if userInfo[3].lower() == 'e':
-				educ = "ensino medio"
-			else if userInfo[3].lower() == 'g':
-				educ = "graduacao"
-			else if userInfo[3].lower() == 'm':
-				educ = "mestrado"
-			else if userInfo[3].lower() == 'd':
-				educ = "doutorado"
+				educ = ""
+				if userInfo[3][0].lower() == 'e':
+					educ = "ensino medio"
+				elif userInfo[3][0].lower() == 'g':
+					educ = "graduacao"
+				elif userInfo[3][0].lower() == 'm':
+					educ = "mestrado"
+				elif userInfo[3][0].lower() == 'd':
+					educ = "doutorado"
 
-			#age, gender, income, education, city, marital status
-			usersDef[userID] = [userInfo[0], userInfo[1][0].lower(), income, educ, userInfo[4].lower(), userInfo[6].lower()]
+				#User profile: age, gender, income, education, city, marital status
+				usersDef[userID] = [userInfo[0].strip(' \t\n\r'), gender, income, educ, userInfo[4].lower(), userInfo[6].lower()]
+	
 	return usersDef
 
 
 def readImagesDefinitions(lines):
-	#TODO! image_url,  street_wid, mov_cars, park_cars, mov_ciclyst, landscape, build_ident, trees, build_height, diff_build, people, graffiti, bairro)
 	imagesDef = {}
 	for line in lines:
 		data = line.split(" ")
-		imagesDef[data[0]] = [data[1:]]
+		imagesDef[data[0]] = data[1:]
 
 	return imagesDef
 

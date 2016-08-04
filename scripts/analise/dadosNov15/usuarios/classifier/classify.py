9bae46173b6d7132578996897addae176e16d3a7
David <david@david-HP.(none)>
2016-08-04 09:25:28 -0300

    Changing to use pandas

--- scripts/analise/dadosNov15/usuarios/classifier/classify.py
+++ scripts/analise/dadosNov15/usuarios/classifier/classify.py
@@ -110,11 +110,11 @@ def convertColumnsToDummy(df):
 	#Users categorical information to dummy!	
 	res = pd.get_dummies(df['gender'])
 	df.join(res)
-	res = pdf.get_dummies(df['income'])
+	res = pd.get_dummies(df['income'])
 	df.join(res)
-	res = pdf.get_dummies(df['marital'])
+	res = pd.get_dummies(df['marital'])
 	df.join(res)
-	res = pdf.get_dummies(df['education'])
+	res = pd.get_dummies(df['education'])
 	df.join(res)
 
 	#Images categorical information to dummy!
@@ -129,112 +129,90 @@ def convertColumnsToDummy(df):
 	
 	return df
 
+def train_classifiers(question, predictors, answer, parameters_dic, classifiers_names, classifiers):
+	#Question being evaluated
+	print ">>>>>> " + question
+
+	i = 0
+	predictors = np.array(predictors)
+	answer = np.array(answer)
+	for classifier_index in range(0, len(classifiers)):
+
+		print "### Classifier " + str(classifiers_names[classifierIndex])
+		if parameters_dic.has_key(classifiers_names[classifierIndex]):
+			parameters_to_optimize = parameters_dic[classifiers_names[classifier_index]]
+			print "### Param to opt " + str(parameters_to_optimize)
+
+			for train, test in StratifiedKFold(answer, n_folds=5): #5folds
+
+				classifier = GridSearchCV(classifiers[classifier_index], 
+				      param_grid=parameters_to_optimize, cv=3)
+				predictors_train = predictors[train]
+				answer_train = answer[train]
+				clf = classifier.fit(predictors_train, answer_train)
+
+				i += 1
+				print('Fold', i)
+				print(clf.best_estimator_)
+				print()
+		
+				predictors_test = predictors[test]
+				answer_test = answer[test]
+				y_pred = clf.predict(predictors_test)
+
+				#Vamo ver o F1. To usando micro, pode ser o macro. No paper, tem que mostrar os 2 mesmo.
+				print('F1 score no teste, nunca use isto para escolher parametros. ' + \
+				  'Aceite o valor, tuning de parametros so antes com o grid search', 
+				  f1_score(answer_test, y_pred, average='micro'))
+				print()
+				print()
+
+
+
 if __name__ == "__main__":
-	if len(sys.argv) < 2:
+	if len(sys.argv) < 3:
 		print "Uso: <arquivo com dados das preferencias de fotos, dados das fotos e dos usuarios> <phase - train or test> <filter, e.g., <gender, marital, age, income>-masculino>"
 		sys.exit(1)
 
-	#df = pd.read_table(sys.argv[1], sep='\t', encoding='utf8', header=0)
-	#phase = sys.argv[2].lower()
-	#if len(sys.argv) == 3:
-	#	filter_group = sys.argv[3]
-	#	group = filter_group.split("-")[1]
-	#else:
-	#	filter_group = ""
-	#	group = ""
-	#if len(filterGroup) > 0:
-	#	if 'gender' in filterGroup:
-	#		df_to_use = df[(df.gender == group)]
-	#	elif 'marital' in filterGroup:
-	#		df_to_use = df[(df.marital == group)]
-	#	elif 'income' in filterGroup:
-	#		if group == 'media':		
-	#			df_to_use = df[(df.income == "media") or (df.income == "media alta")]
-	#		elif group == 'baixa':
-	#			df_to_use = df[(df.income == "baixa") or (df.income == "media baixa")]
-	#	elif 'age' in filterGroup:
-	#		if age == 'adulto':
-	#			df_to_use = df[(df.age >= 25)]
-	#		elif age == 'jovem':
-	#			df_to_use = df[(df.age >= 24)]
-	#else:
-	#	df_to_use = df
-	#agrad_df = df_to_use[(df_to_use.question != "seguro?")]
-	#agrad_df = convertColumnsToDummy(agrad_df)
-	#answerAgrad = agrad_df['choice']    .append(int(lineData[3]))#Preferred image
-	#predictorsAgrad = agrad_df[['age', 'masculino', 'feminino', 'baixa', 'media baixa', 'media', 'media alta', 'graduacao', 'mestrado', 'doutorado', 'ensino medio', 'solteiro', 'casado', 'divorciado', 'vi\u00favo', 'street_wid1', 'mov_cars1', 'park_cars1', 'mov_ciclyst1', 'landscape1', 'build_ident1', 'trees1', 'build_height1', 'diff_build1', 'people1', 'graffiti1_No', 'graffiti1_Yes', 'bairro1_catole', 'bairro1_centro', 'bairro1_liberdade', 'mov_cars2', 'park_cars2', 'mov_ciclyst2', 'landscape2', 'build_ident2', 'trees2', 'build_height2', 'diff_build2', 'people2', 'graffiti2_No', 'graffiti2_Yes', 'bairro2_catole', 'bairro2_centro', 'bairro2_liberdade']].values #Predictors
-	#seg_df = df_to_use[(df_to_use.question == "seguro?")]
-	#seg_df = convertColumnsToDummy(seg_df)
-	#answerSeg = seg_df['choice']    .append(int(lineData[3]))#Preferred image
-	#predictorsSeg = seg_df[['age', 'masculino', 'feminino', 'baixa', 'media baixa', 'media', 'media alta', 'graduacao', 'mestrado', 'doutorado', 'ensino medio', 'solteiro', 'casado', 'divorciado', 'vi\u00favo', 'street_wid1', 'mov_cars1', 'park_cars1', 'mov_ciclyst1', 'landscape1', 'build_ident1', 'trees1', 'build_height1', 'diff_build1', 'people1', 'graffiti1_No', 'graffiti1_Yes', 'bairro1_catole', 'bairro1_centro', 'bairro1_liberdade', 'mov_cars2', 'park_cars2', 'mov_ciclyst2', 'landscape2', 'build_ident2', 'trees2', 'build_height2', 'diff_build2', 'people2', 'graffiti2_No', 'graffiti2_Yes', 'bairro2_catole', 'bairro2_centro', 'bairro2_liberdade']].values #Predictors
+	df = pd.read_table(sys.argv[1], sep='\t', encoding='utf8', header=0)
+	phase = sys.argv[2].lower()
 
-	
-	#Reading data for Como e Campina images!
-	inputLines = open(sys.argv[1], 'r').readlines()
-	answerAgrad = []
-	predictorsAgrad = []
-	answerSeg = []
-	predictorsSeg = []
-
-	if len(sys.argv) == 3:
-		filterGroup = sys.argv[2]
-		group = filterGroup.split("-")[1]
+	if len(sys.argv) == 4:
+		filter_group = sys.argv[3]
+		group = filter_group.split("-")[1]
 	else:
-		filterGroup = ""
+		filter_group = ""
 		group = ""
 
-	for line in inputLines[1:]:
-		lineData = line.split("\t")
-		question = lineData[0]
+	if len(filter_group) > 0:
+		if 'gender' in filterGroup:
+			df_to_use = df[(df.gender == group)]
+		elif 'marital' in filterGroup:
+			df_to_use = df[(df.marital == group)]
+		elif 'income' in filterGroup:
+			if group == 'media':		
+				df_to_use = df[(df.income == "media") or (df.income == "media alta")]
+			elif group == 'baixa':
+				df_to_use = df[(df.income == "baixa") or (df.income == "media baixa")]
+		elif 'age' in filterGroup:
+			if age == 'adulto':
+				df_to_use = df[(df.age >= 25)]
+			elif age == 'jovem':
+				df_to_use = df[(df.age >= 24)]
+	else:
+		df_to_use = df
 
-		userInfoConverted = convertUserInfo(lineData[5:11])
-		#userInfoConverted = []
-		imageInfoConverted = convertImageInfo(lineData[11:])
-		addExecution = False
+	agrad_df = df_to_use[(df_to_use.question != "seguro?")]
+	agrad_df = convertColumnsToDummy(agrad_df)
+	answer_agrad = agrad_df['choice']#Preferred images
+	predictors_agrad = agrad_df[['age', 'masculino', 'feminino', 'baixa', 'media baixa', 'media', 'media alta', 'graduacao', 'mestrado', 'doutorado', 'ensino medio', 'solteiro', 'casado', 'divorciado', 'vi\u00favo', 'street_wid1', 'mov_cars1', 'park_cars1', 'mov_ciclyst1', 'landscape1', 'build_ident1', 'trees1', 'build_height1', 'diff_build1', 'people1', 'graffiti1_No', 'graffiti1_Yes', 'bairro1_catole', 'bairro1_centro', 'bairro1_liberdade', 'mov_cars2', 'park_cars2', 'mov_ciclyst2', 'landscape2', 'build_ident2', 'trees2', 'build_height2', 'diff_build2', 'people2', 'graffiti2_No', 'graffiti2_Yes', 'bairro2_catole', 'bairro2_centro', 'bairro2_liberdade']].values #Predictors
 
-		#Filtering groups to evaluate
-		if len(filterGroup) > 0:
-			#print str(userInfoConverted[1]) + " " + group + " " + str(userInfoConverted[1] == group)
-			if 'gender' in filterGroup and userInfoConverted[1] == genderDic[group]:
-				userInfoConverted.extend(imageInfoConverted)
-				addExecution = True
-			elif 'marital' in filterGroup and userInfoConverted[4] == maritalDic[group]:
-				userInfoConverted.extend(imageInfoConverted)
-				addExecution = True
-			elif 'age' in filterGroup and ( (group == 'jovem' and userInfoConverted[0] <= 24) or (group == 'adulto' and userInfoConverted[0] >= 25) ):
-				userInfoConverted.extend(imageInfoConverted)
-				addExecution = True
-			elif 'income' in filterGroup and ( (group == 'media' and (userInfoConverted[2] == incomeDic['media'] or userInfoConverted[2] == incomeDic['media alta'])) or (group == 'baixa' and (userInfoConverted[2] == incomeDic['baixa'] or userInfoConverted[2] == incomeDic['media baixa'])) ):	
-				userInfoConverted.extend(imageInfoConverted)
-				addExecution = True
-		elif len(filterGroup) == 0:
-			userInfoConverted.extend(imageInfoConverted)
-			addExecution = True
+	seg_df = df_to_use[(df_to_use.question == "seguro?")]
+	seg_df = convertColumnsToDummy(seg_df)
+	answer_seg = seg_df['choice']#Preferred images
+	predictors_seg = seg_df[['age', 'masculino', 'feminino', 'baixa', 'media baixa', 'media', 'media alta', 'graduacao', 'mestrado', 'doutorado', 'ensino medio', 'solteiro', 'casado', 'divorciado', 'vi\u00favo', 'street_wid1', 'mov_cars1', 'park_cars1', 'mov_ciclyst1', 'landscape1', 'build_ident1', 'trees1', 'build_height1', 'diff_build1', 'people1', 'graffiti1_No', 'graffiti1_Yes', 'bairro1_catole', 'bairro1_centro', 'bairro1_liberdade', 'mov_cars2', 'park_cars2', 'mov_ciclyst2', 'landscape2', 'build_ident2', 'trees2', 'build_height2', 'diff_build2', 'people2', 'graffiti2_No', 'graffiti2_Yes', 'bairro2_catole', 'bairro2_centro', 'bairro2_liberdade']].values #Predictors
 
-		#Grouping per question
-		if 'agrad' in question.lower() and addExecution:
-			answerAgrad.append(int(lineData[3]))#Preferred image
-			predictorsAgrad.append(userInfoConverted)#Predictors
-		elif addExecution:
-			answerSeg.append(int(lineData[3]))#Preferred image
-			predictorsSeg.append(userInfoConverted)#Predictors
-
-	#Building classifiers
-	#classifiersNames = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Decision Tree",
-        # "Random Forest", "AdaBoost", "Naive Bayes", "Linear Discriminant Analysis",
-        # "Quadratic Discriminant Analysis"]
-
-	#classifiers = [
-	#    KNeighborsClassifier(3),
-	#    SVC(kernel="linear", C=0.025),
-	#    SVC(gamma=2, C=1),
-	#    DecisionTreeClassifier(max_depth=5),
-	#    RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
-	#    AdaBoostClassifier(),
-	#    GaussianNB(),
-	#    LinearDiscriminantAnalysis(),
-	#    QuadraticDiscriminantAnalysis()]
-
+	#Classifiers to be used
 	parameters_dic = { "Extra Trees" : {
 	    'n_estimators': [20, 40, 60],
 	    'criterion': ['entropy'],
@@ -255,158 +233,34 @@ if __name__ == "__main__":
 		'class_weight' : ['balanced', None]
 	}
 	}
-	classifiersNames = ["Linear SVM", "RBF SVM", "Naive Bayes"] #["Extra Trees", "Nearest Neighbors", 
+	classifiers_names = ["Linear SVM", "RBF SVM", "Naive Bayes"] #["Extra Trees", "Nearest Neighbors", 
 	classifiers = [SVC(kernel="linear", C=0.025), SVC(gamma=2, C=1), GaussianNB()]#ExtraTreesClassifier(n_jobs=-1, criterion='entropy'), KNeighborsClassifier(3),  ]
 	
-	#Pleasantness
-	print ">>>>>> Pleasantness "
+	if phase == 'train':
+		train_classifiers("Pleasantness", predictors_agrad, answer_agrad, parameters_dic, classifiers_names, classifiers)
+		train_classifiers("Safety", predictors_seg, answer_seg, parameters_dic, classifiers_names, classifiers)
+	elif phase == 'test':
 
-	i = 0
-	predictorsAgrad = np.array(predictorsAgrad)
-	answerAgrad = np.array(answerAgrad)
-	for classifierIndex in range(0, len(classifiers)):
+		for pair in [ ["Pleasantness", predictors_agrad, answer_agrad], ["Safety", predictors_seg, answer_seg] ]:
+			for classifier_index in range(0, len(classifiers)):
+				clf = classifiers[classifier_index]
+				clf_name = classifiers_names[classifier_index]
 
-		print "### Classifier " + str(classifiersNames[classifierIndex])
-		if parameters_dic.has_key(classifiersNames[classifierIndex]):
-			parameters_to_optimize = parameters_dic[classifiersNames[classifierIndex]]
-			print "### Param to opt " + str(parameters_to_optimize)
+				#Ok, parece que min_samples_leaf=16, min_samples_split=16 e uma boa. Quais as features importantes?
+				#Vou retreinar na base toda e ver. Note que nao vou avaliar nada agora. Poderia fazer a mesma coisa
+				#que fiz aqui para cada fold acima e tirar a media, e outra abordagem.
+				#clf = ExtraTreesClassifier(min_samples_leaf=16, min_samples_split=16)
+				clf.fit(pair[1], pair[2])
 
-			for train, test in StratifiedKFold(answerAgrad, n_folds=5): #5folds
+				#Feature importances me diz a importancia de cada feature. Maior == mais importante.
+				#Depois voce pode mapear para o nome das suas features
+				print ">>>> " + pair[0] + " " + clf_name
+				print(clf.feature_importances_)
+	else:
+		print "Phase not selected correctly: train or test!"
+		sys.exit(1)
 
-				classifier = GridSearchCV(classifiers[classifierIndex], 
-				      param_grid=parameters_to_optimize, cv=3)
-				predictorsTrain = predictorsAgrad[train]
-				answerTrain = answerAgrad[train]
-				clf = classifier.fit(predictorsTrain, answerTrain)
-
-				i += 1
-				print('Fold', i)
-				print(clf.best_estimator_)
-				print()
-		
-				predictorsTest = predictorsAgrad[test]
-				answerTest = answerAgrad[test]
-				y_pred = clf.predict(predictorsTest)
-
-				#Vamo ver o F1. To usando micro, pode ser o macro. No paper, tem que mostrar os 2 mesmo.
-				print('F1 score no teste, nunca use isto para escolher parametros. ' + \
-				  'Aceite o valor, tuning de parametros so antes com o grid search', 
-				  f1_score(answerTest, y_pred, average='micro'))
-				print()
-				print()
-
-
-	#Ok, parece que min_samples_leaf=16, min_samples_split=16 e uma boa. Quais as features importantes?
-	#Vou retreinar na base toda e ver. Note que nao vou avaliar nada agora. Poderia fazer a mesma coisa
-	#que fiz aqui para cada fold acima e tirar a media, e outra abordagem.
-	clf = ExtraTreesClassifier(min_samples_leaf=16, min_samples_split=16)
-	clf.fit(predictorsAgrad, answerAgrad)
-
-	#Feature importances me diz a importancia de cada feature. Maior == mais importante.
-	#Depois voce pode mapear para o nome das suas features
-	print(clf.feature_importances_)
-
-	#indexesToTrain = random.sample( range(0,len(answerAgrad)), int(len(answerAgrad)*train_percent) )#Separating training data and test data!
-	#indexesToTest = list( set(range(0, len(answerAgrad))) - set(indexesToTrain) )
-
-	#Testing composition
-	#testComposition(indexesToTrain, predictorsAgrad, "TRAIN")  
-	#testComposition(indexesToTest, predictorsAgrad, "TEST")  
-
-	#Creating train and test datasets
-	#predictorsInput = np.array(predictorsAgrad)[indexesToTrain]
-	#answersInput = np.array(answerAgrad)[indexesToTrain]
-	#predictorsOutput = np.array(predictorsAgrad)[indexesToTest]
-	#answersOutput = np.array(answerAgrad)[indexesToTest]
-
-	#Iterate over classifiers
-	#print ">>>>>> Pleasantness " + str(len(indexesToTrain)) + " " + str(len(indexesToTest)) 
-	#print "Classifier\tmean accuracy\t(precision,\trecall,\tf1)"
-	#for name, clf in zip(classifiersNames, classifiers):
-		#clf.fit(predictorsInput, answersInput)
-		#y_pred = clf.fit( predictorsInput, answersInput ).predict( predictorsOutput )#Predicted values
-		#score = clf.score(predictorsOutput, answersOutput)#Accuracy score
-		#myAcc = (answersOutput == y_pred).sum() * 1.0 / (len(y_pred))
-		#myAcc = accuracy_score(answersOutput, y_pred)
-		#print "CORRE " + str(myAcc)
-
-		#metrics = precision_recall_fscore_support(answersOutput, y_pred, average='macro', labels=['1', '0', '-1'])#Calculates for each label and compute the mean!
-		#print name + " " + str(score) + " MACRO " + str(metrics)
-		#metrics = precision_recall_fscore_support(answersOutput, y_pred, average='micro', labels=['1', '0', '-1'])#Total false positives, negatives and true positives -> more similar to accuracy
-		#print name + " " + str(score) + " MICRO " + str(metrics)
-
-	#Safety
-	print ">>>>>> Safety "
-	#indexesToTrain = random.sample( range(0,len(answerSeg)), int(len(answerSeg)*train_percent) )#Separating training data and test data!
-	#indexesToTest = list( set(range(0, len(answerSeg))) - set(indexesToTrain) )
-
-	#Testing composition
-	#testComposition(indexesToTrain, predictorsSeg, "TRAIN")
-	#testComposition(indexesToTest, predictorsSeg, "TEST")    
-
-	#Creating train and test datasets
-	#predictorsInput = np.array(predictorsSeg)[indexesToTrain]
-	#answersInput = np.array(answerSeg)[indexesToTrain]
-	#predictorsOutput = np.array(predictorsSeg)[indexesToTest]
-	#answersOutput = np.array(answerSeg)[indexesToTest]
-
-	#Iterate over classifiers
-	#print ">>>>>> Safety " + str(len(indexesToTrain)) + " " + str(len(indexesToTest)) 
-	#print "Classifier\tmean accuracy\t(precision,\trecall,\tf1)"
-	#for name, clf in zip(classifiersNames, classifiers):
-#		clf.fit(predictorsInput, answersInput)
-#		y_pred = clf.fit( predictorsInput, answersInput ).predict( predictorsOutput )
-#		score = clf.score(predictorsOutput, answersOutput)
-#
-#		metrics = precision_recall_fscore_support(answersOutput, y_pred, average='macro', labels=['1', '0', '-1'])#Calculates for each label and compute the mean!
-#		print name + " " + str(score) + " MACRO " + str(metrics)
-#		metrics = precision_recall_fscore_support(answersOutput, y_pred, average='micro', labels=['1', '0', '-1'])#Total false positives, negatives and true positives -> more similar to accuracy
-#		print name + " " + str(score) + " MICRO " + str(metrics)
-	i = 0
-	predictorsSeg = np.array(predictorsSeg)
-	answerSeg = np.array(answerSeg)
-	for classifierIndex in range(0, len(classifiers)):
-
-		print "### Classifier " + str(classifiersNames[classifierIndex])
-		if parameters_dic.has_key(classifiersNames[classifierIndex]):
-			parameters_to_optimize = parameters_dic[classifiersNames[classifierIndex]]
-
-			for train, test in StratifiedKFold(answerSeg, n_folds=5): #5folds
-
-				classifier = GridSearchCV(classifiers[classifierIndex], 
-				      param_grid=parameters_to_optimize, cv=3)
-				predictorsTrain = predictorsSeg[train]
-				answerTrain = answerSeg[train]
-				clf = classifier.fit(predictorsTrain, answerTrain)
-
-				i += 1
-				print('Fold', i)
-				print(clf.best_estimator_)
-				print()
-		
-				predictorsTest = predictorsSeg[test]
-				answerTest = answerSeg[test]
-				y_pred = clf.predict(predictorsTest)
-
-				#Vamo ver o F1. To usando micro, pode ser o macro. No paper, tem que mostrar os 2 mesmo.
-				print('F1 score no teste, nunca use isto para escolher parametros. ' + \
-				  'Aceite o valor, tuning de parametros so antes com o grid search', 
-				  f1_score(answerTest, y_pred, average='micro'))
-				print()
-				print()
-
-
-
-	clf = ExtraTreesClassifier(min_samples_leaf=16, min_samples_split=16)
-	clf.fit(predictorsSeg, answerSeg)
-
-	#Feature importances me diz a importancia de cada feature. Maior == mais importante.
-	#Depois voce pode mapear para o nome das suas features
-	print(clf.feature_importances_)
-
-
-
-
+	
 	#>>>> Example with iris dataset
 	#iris = datasets.load_iris()
 	#print str(iris.data) + " " + str(len(iris.data))

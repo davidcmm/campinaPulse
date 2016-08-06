bb24bd4b71fc74d5bdf3269cbf620dac621eba3f
davidcmm <davcandeia@gmail.com>
2016-08-06 11:15:19 -0300

    Changing output names and fixing scripts

--- scripts/analise/dadosNov15/usuarios/classifier/executaTestGrupos.sh
+++ scripts/analise/dadosNov15/usuarios/classifier/executaTestGrupos.sh
@@ -2,19 +2,19 @@
 
 echo "#### Only with urban elements features ####" >> classifier_output_groups_wo_user_features.dat
 echo ">> Masculino" >> classifier_output_groups_wo_user_features.dat
-python classify4.py classifier_input.dat importances gender-masculino >> classifier_output_groups_wo_user_features.dat
+python classify.py classifier_input.dat test gender-masculino >> classifier_output_groups_wo_user_features.dat
 echo ">> Feminino" >> classifier_output_groups_wo_user_features.dat
-python classify4.py classifier_input.dat importances gender-feminino >> classifier_output_groups_wo_user_features.dat
+python classify.py classifier_input.dat test gender-feminino >> classifier_output_groups_wo_user_features.dat
 echo ">> Jovem" >> classifier_output_groups_wo_user_features.dat
-python classify4.py classifier_input.dat importances age-jovem >> classifier_output_groups_wo_user_features.dat
+python classify.py classifier_input.dat test age-jovem >> classifier_output_groups_wo_user_features.dat
 echo ">> Adulto" >> classifier_output_groups_wo_user_features.dat
-python classify4.py classifier_input.dat importances age-adulto >> classifier_output_groups_wo_user_features.dat
+python classify.py classifier_input.dat test age-adulto >> classifier_output_groups_wo_user_features.dat
 echo ">> Baixa" >> classifier_output_groups_wo_user_features.dat
-python classify4.py classifier_input.dat importances income-baixa >> classifier_output_groups_wo_user_features.dat
+python classify.py classifier_input.dat test income-baixa >> classifier_output_groups_wo_user_features.dat
 echo ">> Media" >> classifier_output_groups_wo_user_features.dat
-python classify4.py classifier_input.dat importances income-media >> classifier_output_groups_wo_user_features.dat
+python classify.py classifier_input.dat test income-media >> classifier_output_groups_wo_user_features.dat
 echo ">> Solteiro" >> classifier_output_groups_wo_user_features.dat
-python classify4.py classifier_input.dat importances marital-solteiro >> classifier_output_groups_wo_user_features.dat
+python classify.py classifier_input.dat test marital-solteiro >> classifier_output_groups_wo_user_features.dat
 echo ">> Casado" >> classifier_output_groups_wo_user_features.dat
-python classify4.py classifier_input.dat importances marital-casado >> classifier_output_groups_wo_user_features.dat
+python classify.py classifier_input.dat test marital-casado >> classifier_output_groups_wo_user_features.dat
 

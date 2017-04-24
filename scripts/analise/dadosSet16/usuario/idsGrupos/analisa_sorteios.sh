00e4e569f8e7a94be848baf278b796c0aeaf0db2
davidcmm <davcandeia@gmail.com>
2017-04-24 17:08:08 -0300

    Updating scripts

--- scripts/analise/dadosSet16/usuario/idsGrupos/analisa_sorteios.sh
+++ scripts/analise/dadosSet16/usuario/idsGrupos/analisa_sorteios.sh
@@ -1,69 +1,89 @@
 #!/bin/bash
 #Selects Como é Campina? answers based on users ids, per randomized group, and then computes Q-Scores for selected answers
 
-#python sorteia_grupos.py igual feminino.dat masculino.dat 100 #Creates ids files from second file with same size of first file
-python sorteia_grupos.py desb 60 15 feminino.dat masculino.dat 100 #Creates ids files from each file according to corresponding sizes specified first
 
-#Iterating through each generated men files and comparing to a reference women file
-#for file in `ls masculino_*.dat`; do
-#
-#	currentFile=`echo $file | cut -d \_ -f 2 | cut -d . -f 1`
-#	python ../selectRunPerUsers.py ../run100/run.csv $file run > runMasculino_${currentFile}.csv
-#	
-#	python ../analisaQScore.py runMasculino_${currentFile}.csv 100 ../tasksDef.csv campina
-#	mv all.dat allMasculino_${currentFile}.dat
-#
-#	sort -k 3 -r -o allMasculino_${currentFile}.dat allMasculino_${currentFile}.dat
-#
-#	python ../encontraInterseccao.py ../all100/allFemininoOrd.dat allMasculino_${currentFile}.dat > intersectionAllFemMas_${currentFile}.dat 
-#	python ../preparaHTML.py ../all100/allFemininoOrd.dat intersectionAllFemMas_${currentFile}.dat > allFemininoOrdInter_${currentFile}.dat 
-#	python ../preparaHTML.py allMasculino_${currentFile}.dat intersectionAllFemMas_${currentFile}.dat > allMasculinoOrdInter_${currentFile}.dat 
-#	rm question.html
-#
-	#Building all evaluations with only selected men, this means removing men that were not sorted in current group
-#	python sorteia_grupos_geral.py masculino masculino_${currentFile}.dat
-#	python ../selectRunPerUsers.py ../run100/run.csv homMul_95/diff_masculino_${currentFile}.dat remove > homMul_95/runDiffMasculino_${currentFile}.csv
-#	python ../analisaQScore.py homMul_95/runDiffMasculino_${currentFile}.csv 100 ../tasksDef.csv campina
-#	mv all.dat homMul_95/allDiffMasculino_${currentFile}.dat
-#
-#	sort -k 3 -r -o homMul_95/allDiffMasculino_${currentFile}.dat homMul_95/allDiffMasculino_${currentFile}.dat
-#done
 
-#Iterating through each generated men and women files
-for i in {0..99}; do 
+for group in "masculino-feminino"; do #"jovem-adulto" "media-baixa" ; do #"masculino-feminino" "jovem-adulto" "media-baixa"
 
-	#currentFile=`echo $file | cut -d \_ -f 2 | cut -d . -f 1`
-	python ../selectRunPerUsers.py ../run100/run.csv masculino_${i}.dat run > runMasculino_${i}.csv
-	python ../selectRunPerUsers.py ../run100/run.csv feminino_${i}.dat run > runFeminino_${i}.csv
+
+	group1=`echo $group | cut -d \- -f 1`
+	group2=`echo $group | cut -d \- -f 2`
+
+	for desb in "60-15" ; do #"60-40" "15-60" "40-60" ; do
+
+		size1=`echo $desb | cut -d \- -f 1`
+		size2=`echo $desb | cut -d \- -f 2`
+
+		mkdir ${group1}${size1}_${group2}${size2}
 	
-	python ../analisaQScore.py runMasculino_${i}.csv 100 ../tasksDef.csv campina
-	mv all.dat allMasculino_${i}.dat
-	python ../analisaQScore.py runFeminino_${i}.csv 100 ../tasksDef.csv campina
-	mv all.dat allFeminino_${i}.dat
+		#python sorteia_grupos.py igual feminino.dat masculino.dat 100 #Creates ids files from second file with same size of first file
+		python sorteia_grupos.py desb ${size2} ${size1} ${group2}.dat ${group1}.dat 100 #Creates ids files from each file according to corresponding sizes specified first
 
-	sort -k 3 -r -o allMasculino_${i}.dat allMasculino_${i}.dat
-	sort -k 3 -r -o allFeminino_${i}.dat allFeminino_${i}.dat
+		#Iterating through each generated men files and comparing to a reference women file
+		#for file in `ls masculino_*.dat`; do
+		#
+		#	currentFile=`echo $file | cut -d \_ -f 2 | cut -d . -f 1`
+		#	python ../selectRunPerUsers.py ../run100/run.csv $file run > runMasculino_${currentFile}.csv
+		#	
+		#	python ../analisaQScore.py runMasculino_${currentFile}.csv 100 ../tasksDef.csv campina
+		#	mv all.dat allMasculino_${currentFile}.dat
+		#
+		#	sort -k 3 -r -o allMasculino_${currentFile}.dat allMasculino_${currentFile}.dat
+		#
+		#	python ../encontraInterseccao.py ../all100/allFemininoOrd.dat allMasculino_${currentFile}.dat > intersectionAllFemMas_${currentFile}.dat 
+		#	python ../preparaHTML.py ../all100/allFemininoOrd.dat intersectionAllFemMas_${currentFile}.dat > allFemininoOrdInter_${currentFile}.dat 
+		#	python ../preparaHTML.py allMasculino_${currentFile}.dat intersectionAllFemMas_${currentFile}.dat > allMasculinoOrdInter_${currentFile}.dat 
+		#	rm question.html
+		#
+			#Building all evaluations with only selected men, this means removing men that were not sorted in current group
+		#	python sorteia_grupos_geral.py masculino masculino_${currentFile}.dat
+		#	python ../selectRunPerUsers.py ../run100/run.csv homMul_95/diff_masculino_${currentFile}.dat remove > homMul_95/runDiffMasculino_${currentFile}.csv
+		#	python ../analisaQScore.py homMul_95/runDiffMasculino_${currentFile}.csv 100 ../tasksDef.csv campina
+		#	mv all.dat homMul_95/allDiffMasculino_${currentFile}.dat
+		#
+		#	sort -k 3 -r -o homMul_95/allDiffMasculino_${currentFile}.dat homMul_95/allDiffMasculino_${currentFile}.dat
+		#done
 
-	python ../encontraInterseccao.py allFeminino_${i}.dat allMasculino_${i}.dat > intersectionAllFemMas_${i}.dat 
-	python ../preparaHTML.py allFeminino_${i}.dat intersectionAllFemMas_${i}.dat > allFemininoOrdInter_${i}.dat 
-	rm question.html
-	python ../preparaHTML.py allMasculino_${i}.dat intersectionAllFemMas_${i}.dat > allMasculinoOrdInter_${i}.dat 
-	rm question.html
+		#Iterating through each generated men and women files
+		for i in {0..99}; do 
 
-	#Building all evaluations with only selected men, this means removing men that were not sorted in current group
-	python sorteia_grupos_geral.py masculino masculino_${i}.dat
-	python sorteia_grupos_geral.py feminino feminino_${i}.dat
+			#currentFile=`echo $file | cut -d \_ -f 2 | cut -d . -f 1`
+			python ../selectRunPerUsers.py ../run100/run.csv ${group1}_${i}.dat run > run${group1}_${i}.csv
+			python ../selectRunPerUsers.py ../run100/run.csv ${group2}_${i}.dat run > run${group2}_${i}.csv
+	
+			python ../analisaQScore.py run${group1}_${i}.csv 100 ../tasksDef.csv campina
+			mv all.dat all${group1}_${i}.dat
+			python ../analisaQScore.py run${group2}_${i}.csv 100 ../tasksDef.csv campina
+			mv all.dat all${group2}_${i}.dat
 
-	python ../selectRunPerUsers.py ../run100/run.csv diff_masculino_${i}.dat diff_feminino_${i}.dat remove > runDiffMasFem_${i}.csv
-	#python ../selectRunPerUsers.py ../run100/run.csv diff_feminino_${i}.dat remove > runDiffFeminino_${i}.csv
+			sort -k 3 -r -o all${group1}_${i}.dat all${group1}_${i}.dat
+			sort -k 3 -r -o all${group2}_${i}.dat all${group2}_${i}.dat
 
-	python ../analisaQScore.py runDiffMasFem_${i}.csv 100 ../tasksDef.csv campina
-	mv all.dat allDiffMasFem_${i}.dat
-	#python ../analisaQScore.py runDiffFeminino_${i}.csv 100 ../tasksDef.csv campina
-	#mv all.dat allDiffFeminino_${i}.dat
+			python ../encontraInterseccao.py all${group2}_${i}.dat all${group1}_${i}.dat > intersectionAll${group2}${group1}_${i}.dat 
+			python ../preparaHTML.py all${group2}_${i}.dat intersectionAll${group2}${group1}_${i}.dat > all${group2}OrdInter_${i}.dat 
+			rm question.html
+			python ../preparaHTML.py all${group1}_${i}.dat intersectionAll${group2}${group1}_${i}.dat > all${group1}OrdInter_${i}.dat 
+			rm question.html
 
-	sort -k 3 -r -o allDiffMasFem_${i}.dat allDiffMasFem_${i}.dat
-	#sort -k 3 -r -o allDiffFeminino_${i}.dat allDiffFeminino_${i}.dat
+			#Building all evaluations with only selected men, this means removing men that were not sorted in current group
+			python sorteia_grupos_geral.py ${group1} ${group1}_${i}.dat
+			python sorteia_grupos_geral.py ${group2} ${group2}_${i}.dat
+
+			python ../selectRunPerUsers.py ../run100/run.csv diff_${group1}_${i}.dat diff_${group2}_${i}.dat remove > runDiff${group2}${group1}_${i}.csv
+			#python ../selectRunPerUsers.py ../run100/run.csv diff_feminino_${i}.dat remove > runDiffFeminino_${i}.csv
+
+			python ../analisaQScore.py runDiff${group2}${group1}_${i}.csv 100 ../tasksDef.csv campina
+			mv all.dat allDiff${group2}${group1}_${i}.dat
+			#python ../analisaQScore.py runDiffFeminino_${i}.csv 100 ../tasksDef.csv campina
+			#mv all.dat allDiffFeminino_${i}.dat
+
+			sort -k 3 -r -o allDiff${group2}${group1}_${i}.dat allDiff${group2}${group1}_${i}.dat
+			#sort -k 3 -r -o allDiffFeminino_${i}.dat allDiffFeminino_${i}.dat
+
+			mv run*_*.csv all*_*.dat intersectionAll*.dat diff_*.dat ${group1}_*.dat ${group2}_*.dat ${group1}${size1}_${group2}${size2}
+		done
+	done
+
 done
 
 

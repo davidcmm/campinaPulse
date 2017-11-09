90b56db0ed8c6e032a82296782d860913633a895
davidcmm <davcandeia@gmail.com>
2017-11-09 17:49:20 -0300

    Fixing script to add MaxDiff tasks

--- scripts/maxDiff/geraMaxDiffDesignDeListaDeFotos.py
+++ scripts/maxDiff/geraMaxDiffDesignDeListaDeFotos.py
@@ -10,10 +10,14 @@ possibleQuestions = ["agrad%C3%A1vel?", "seguro?"]
 task_size = 4
 photo_threshold = 13
 
-def buildMaxDiff(photos_filename, user_question):
+def buildMaxDiff(photos_filename, user_question, output_filename):
 
 	photos_file = open(photos_filename, "r")
 	lines = photos_file.readlines()
+	photos_file.close()
+
+	output_file = open(output_filename, "w")
+
 	allPhotos = set([])
 	photo_count = {}
 
@@ -69,99 +73,100 @@ def buildMaxDiff(photos_filename, user_question):
 		#Building current task
 		all_tasks.append(set(currentPhotos))
 
-		sys.stdout.write(question)
+		output_file.write(question)
 		for i in range(0, len(currentPhotos)):
-			sys.stdout.write("\t" + currentPhotos[i])
-		sys.stdout.write("\n")
+			output_file.write("\t" + currentPhotos[i])
+		output_file.write("\n")
 		
 		#Checking if each photo reached threshold comparisons
 		for photo in currentPhotos:
 			if photo_count[photo] >= photo_threshold and photo in allPhotos:
 				allPhotos.remove(photo)
 
+
 	#Logging
 	for photo in allPhotos:
-		print "All photos remained with: " + photo
+		output_file.write("All photos remained with: " + photo + "\n")
 
 	for photo in photo_count.keys():
-		print "Counter for " + photo + "\t" + str(photo_count[photo])
+		output_file.write("Counter for " + photo + "\t" + str(photo_count[photo])+"\n")
+	output_file.close()	
 
-def addMaxDiff(photos_filename, user_question, amount_new_photos):
+def addMaxDiff(photos_filename, user_question, amount_new_photos, output_filename, new_output_filename, old_input_file):
+	allPhotos = buildMaxDiff(photos_filename, user_question, output_filename)
+
+	output_file = open(output_filename, "r")
+	output_lines = output_file.readlines()
+	output_file.close()
 
 	photos_file = open(photos_filename, "r")
-	lines = photos_file.readlines()
+	input_lines = photos_file.readlines()
+	photos_file.close()
 
-	allPhotos = set([])
-	photo_count = {}
+	old_photos_file = open(old_input_file, "r")
+	old_input_lines = old_photos_file.readlines()
+	old_photos_file.close()
 
-	if "agrad" in user_question:
-		question = possibleQuestions[0]
-	else:
-		question = possibleQuestions[1]
+	#All photos from input file and selecting new photos
+	allPhotos = []
+	for line in input_lines:
+		allPhotos.append(line.strip(' \t\n\r"'))
+	new_photos = allPhotos[-amount_new_photos:]
 
-	#Reading all photos
-	new_photos = set([])
-	for index in range(0, len(lines)):
-		line = lines[index]
-		photo = line.strip(' \t\n\r"')
-		allPhotos.add(photo)
-		if index >= len(lines) - amount_new_photos:
-			new_photos.add(photo)
-		photo_count[photo] = 0
+	#All photos from previous list
+	old_photos = set([])
+	for line in old_input_lines:
+		old_photos.add(line.strip(' \t\n\r"'))
 
-	newPhotos_bkp = new_photos.copy()
+	new_output_file = open(new_output_filename, "w")
+	
+	#Selecting new photos which tasks should be added and searching for tasks with them
+	new_tasks = set([])
+	for line in output_lines:
+		for new_photo in new_photos:
+			if new_photo in line:
+				new_tasks.add(line.strip(' \t\n\r"')) 
+				continue
 
-	#Building MaxDiff design with task_size photos per task
-	while len(new_photos) > 0:
-		currentPhotos = []
+	for task in new_tasks:
+		if not "Counter" in task:
+			task_data = task.split('\t')
+			for index in range(1, len(task_data)):
+				photo_data = task_data[index]
+				if "foto_" in photo_data:
+					old_photo = random.sample(old_photos, 1)[0]
+					while old_photo in task_data:
+						old_photo = random.sample(old_photos, 1)[0]
+					task_data[index] = old_photo
+			print str(task_data)
+			new_output_file.write(task_data[0]+"\t"+task_data[1]+"\t"+task_data[2]+"\t"+task_data[3]+"\t"+task_data[4]+"\n")
+		else:
+			new_output_file.write(task+"\n")
+	new_output_file.close()
 
-		photos = [random.sample(new_photos, 1)[0]]
-		for i in range(0, task_size - len(photos)):#Retrieve remaining from bkp
-			photo = random.sample(allPhotos, 1)[0]
-			while photo in photos or photo in currentPhotos:
-				photo = random.sample(allPhotos, 1)[0]
-			photos.append(photo)
-
-		#Updating photos counters
-		for photo in photos:
-			if photo_count.has_key(photo):
-				counter = photo_count.get(photo)
-			else:
-				print ">>> Error, photo not found! " + photo
-				sys.exit(1)
-			currentPhotos.append(photo)
-			photo_count[photo] = photo_count[photo]+1
-
-		#Building current task
-		sys.stdout.write(question)
-		for i in range(0, len(currentPhotos)):
-			sys.stdout.write("\t" + currentPhotos[i])
-		sys.stdout.write("\n")
-		
-		#Checking if each photo reached threshold comparisons
-		all_to_check = new_photos.copy()
-		for photo in all_to_check:
-			if photo_count[photo] >= photo_threshold and photo in new_photos:
-				new_photos.remove(photo)
-
-	#Logging
-	for photo in new_photos:
-		print "All photos remained with: " + photo
-
-	for photo in newPhotos_bkp:
-		print "Counter for " + photo + "\t" + str(photo_count[photo])		
+	#Checking generated tasks
+	new_output_file = open(new_output_filename, "r")
+	for line in new_output_file.readlines():
+		if (not "Cristina" in line) and (not "Floriano" in line):
+			print ">>>>> ERROR! LINE " + str(line.strip(' \t\n\r"'))
+		else:
+			print ">>> LINE OK"
+	
 
 if __name__ == "__main__":
-	if len(sys.argv) < 4:
-		print "Uso: <arquivo com lista de fotos> <questao> <adiciona ou novo> <amount of new lines>"
+	if len(sys.argv) < 5:
+		print "Uso: <arquivo com lista de fotos> <questao> <arquivo de saída> <adiciona ou novo> <amount of new lines> <arquivo de saída add> <arquivo com lista antiga>"
 		sys.exit(1)
 
-	action = sys.argv[3]
+	output_filename = sys.argv[3]
+	action = sys.argv[4]
 	if action.lower() == "adiciona":
-		amount = int(sys.argv[4])
-		addMaxDiff(sys.argv[1], sys.argv[2], amount)
+		amount = int(sys.argv[5])
+		new_output_filename = sys.argv[6]
+		old_input_file = sys.argv[7]
+		addMaxDiff(sys.argv[1], sys.argv[2], amount, output_filename, new_output_filename, old_input_file)
 	elif action.lower() == "novo":
-		buildMaxDiff(sys.argv[1], sys.argv[2])
+		buildMaxDiff(sys.argv[1], sys.argv[2], output_filename)
 	else:
 		print ">>>> Error! Action not recognized, it must be <adiciona> or <novo> but you gave " + str(action)
 		sys.exit(1) 

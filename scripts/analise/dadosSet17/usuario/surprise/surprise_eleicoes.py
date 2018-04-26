58fa37a1f362a4de7a89bda0d071a821265c7c02
David Candeia <davcandeia@gmail.com>
2018-04-26 18:16:02 -0300

    Updating surprise for eleicoes

--- scripts/analise/dadosSet17/usuario/surprise/surprise_eleicoes.py
+++ scripts/analise/dadosSet17/usuario/surprise/surprise_eleicoes.py
@@ -69,22 +69,40 @@ def standard_normal(x):
 	mean = 0.0
 	return ( 1.0 / np.sqrt( 2.0 * np.pi * (std**2.0)) ) * np.exp( - ( (x - mean)**2.0 / (2.0 * (std**2.0)) ) ) 
 
-def deMoivre_funnel(votes, people, data, num_of_points):
+def deMoivre_funnel(votes, people, data, num_of_points, debug):
 	total_votes = []
 	#for j in range(0, num_of_points):
         for prop in data:
-		total_votes.append(data[prop][0] * 1.0 / data[prop][1])#Percent for each city
+		total_votes.append(data[prop][0] * 1.0 / data[prop][2])#Percent for each city
 
-	mean_of_votes = np.mean(total_votes)
+	mean_of_votes = np.mean(total_votes)#mean proportion (success of voting in candidate A)
 
 	std_error = np.sqrt( people * mean_of_votes * (1-mean_of_votes) )#np.std(values) / np.sqrt(num_of_values)
 	zs = (votes - (mean_of_votes * people)) / std_error
-	
+
 	integrate_result = integrate.quad(standard_normal, 0, zs)
 	p_de_moivre = 1.0 - ( 2.0 * integrate_result[0] )
 	#print str(std_error) + "\t" + str(zs) + "\t" + str(i) + "\t" + str(integrate_result) + "\t" + str(p_de_moivre)
-	return p_de_moivre
+
+	if debug:
+		print "All votes prop " + str(total_votes)
+		print "Current votes " + str(votes)
+		print "mean votes " + str(mean_of_votes)
+		print "std error " + str(std_error) + " " + str(people)
+		print "zs " + str(zs)
+		print "Integrate " + str(integrate_result)
+		print "De moivre " + str(p_de_moivre)
+
+	return [p_de_moivre, mean_of_votes]
 	
+
+def find_boom_bust():
+	total_votes = []
+	#for j in range(0, num_of_points):
+        for prop in data:
+		total_votes.append(data[prop][0] * 1.0 / data[prop][2])#Percent for each city
+
+	return [np.max(total_votes), np.min(total_votes)]
 
 def calcSurprise(num_of_points):
   
@@ -104,6 +122,8 @@ def calcSurprise(num_of_points):
   uniform_pM = [pMs[0]]
   boom_pM = [pMs[1]]
   bust_pM = [pMs[2]]
+
+  percents = find_boom_bust()
   
   pDMs = [0,0,0]
   pMDs = [0,0,0]
@@ -118,11 +138,13 @@ def calcSurprise(num_of_points):
   sum_diffs = [0,0,0]
 
   #Integration using python (for De moivre) - https://docs.scipy.org/doc/scipy/reference/tutorial/integrate.html
-
-  sum_diffs = [0,0,0]
   for prop in data:
-    
+
       current_row = data[prop]
+      debug = False
+      if prop == 61778:
+	print ">>>> Cheguei com " + str(current_row)
+	debug = True
  
     #Calculate per state surprise
     #for prop in data:
@@ -137,21 +159,29 @@ def calcSurprise(num_of_points):
       #print ">>> Evaluating " + prop + "\t" + str(i)
       #Estimate P(D|M) as 1 - |O - E|
       #uniform
-      #diffs[0] = ((data[prop][i]/total_street) - (avg_street/total_street))
+      result = deMoivre_funnel(current_row[0], current_row[2], data, num_of_points, debug)
+      diffs[0] = ((current_row[0] * 1.0/current_row[2]) - result[1])
       #pDMs[0] = 1 - abs(diffs[0])
-      pDMs[0] = 1.0 / num_of_points * ( num_of_points * deMoivre_funnel(current_row[0], current_row[1], data, num_of_points) )
-      #Average per num
-      #diffs[1] = ((data[prop][i]/total_street) - (avg_num/total_street))
-      pDMs[1] = 1.0 / num_of_points * ( num_of_points * deMoivre_funnel(current_row[0], current_row[1], data, num_of_points) )
-      #normal
-      #diffs[2] = ((data[prop][i]/total_street) - (norm_estimate/total_street))
-      pDMs[2] = 1.0 / num_of_points * ( num_of_points * deMoivre_funnel(current_row[0], current_row[1], data, num_of_points) )
+      pDMs[0] = 1.0 / num_of_points * ( num_of_points *  result[0])
+      #Boom
+      diffs[1] = ((current_row[0]* 1.0/current_row[2]) - percents[0])
+      pDMs[1] = 1 - abs(diffs[1])
+      #Bust
+      diffs[2] = ((current_row[0]* 1.0/current_row[2]) - percents[1])
+      pDMs[2] = 1 - abs(diffs[2])
 
       #Estimate P(M|D)
       #uniform
-      pMDs[0] = pMs[0]*pDMs[0];
-      pMDs[1] = pMs[1]*pDMs[1];
-      pMDs[2] = pMs[2]*pDMs[2];
+      pMDs[0] = pMs[0]*pDMs[0]
+      pMDs[1] = pMs[1]*pDMs[1]
+      pMDs[2] = pMs[2]*pDMs[2]
+
+      if prop == 61778:
+	print "Num points " + str(num_of_points)
+	print "Diffs " + str(diffs)
+	print "PM " + str(pMs)
+	print "PMD " + str(pMDs)
+	print "PDM " + str(pDMs)
       
       
       #Surprise is the sum of KL divergance across model space
@@ -162,6 +192,10 @@ def calcSurprise(num_of_points):
         kl = kl + pMDs[j] * (np.log( pMDs[j] / pMs[j])/np.log(2))
         voteSum = voteSum + diffs[j]*pMs[j]
         sum_diffs[j] = sum_diffs[j] + abs(diffs[j])
+
+        if prop == 61778:
+		print "KL " + str(pMDs[j] * (np.log( pMDs[j] / pMs[j])/np.log(2)))
+		print "VotesSUM " + str(diffs[j]*pMs[j])
       
       if voteSum >= 0:
 	surprise_data[prop] = abs(kl) 
@@ -171,20 +205,24 @@ def calcSurprise(num_of_points):
       uniform_data[prop] = pMs[0]
       base_data[prop] = pMs[1]
     
-      #Now lets globally update our model belief.
-      for j in range(0, len(pMs)):
-        pDMs[j] = 1 - 0.5 * sum_diffs[j]
-        pMDs[j] = pMs[j] * pDMs[j]
-        pMs[j] = pMDs[j]
-    
-      #Normalize
-      summ = np.sum(pMs)
-      for j in range(0, len(pMs)):
-        pMs[j] = pMs[j] / summ
-    
-      uniform_pM.append(pMs[0])
-      boom_pM.append(pMs[1])
-      bust_pM.append(pMs[2])
+  #Now lets globally update our model belief.
+  for j in range(0, len(pMs)):
+	pDMs[j] = 1 - 0.5 * sum_diffs[j]
+	pMDs[j] = pMs[j] * pDMs[j]	
+	pMs[j] = pMDs[j]
+
+  #Normalize
+  summ = np.sum(pMs)
+  for j in range(0, len(pMs)):
+	pMs[j] = pMs[j] / summ
+
+  uniform_pM.append(pMs[0])
+  boom_pM.append(pMs[1])
+  bust_pM.append(pMs[2])
+
+  print ">> Confiança De moivre " + str(uniform_pM)
+  print ">> Confiança boom " + str(boom_pM)
+  print ">> Confiança bust " + str(bust_pM)
 
   return [surprise_data, uniform_data, base_data]
 
